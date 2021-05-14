44110f2bb82e3e38bd172443f9a61d0f0e1c179b
DenTaku <dentaku1412@gmail.com>
2021-05-14 20:43:35 +0900

    AC ABC032C

--- atcoder/abc/abc032/src/bin/c.rs
+++ atcoder/abc/abc032/src/bin/c.rs
@@ -1,10 +1,56 @@
-use proconio::{input, fastout};
+use proconio::{fastout, input};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        k: i64,
+        s: [i64; n]
     }
-    unimplemented!()
+    if k == 0 {
+        if s.into_iter().any(|x| x == 0) {
+            println!("{}", n);
+        } else {
+            println!("0");
+        }
+        return;
+    }
+    let mut best = VecDeque::<i64>::new();
+    let mut now = VecDeque::<i64>::new();
+    let mut mul = 1;
+    let mut flag = false;
+    for e in s {
+        if e == 0 {
+            println!("{}", n);
+            return;
+        }
+        now.push_back(e);
+        // let mut mul = now.iter().fold(1, |mul, el| mul * el);
+        mul *= e;
+        if mul > k {
+            if best.len() < now.len() {
+                let mut tmp = now.clone();
+                tmp.pop_back().unwrap();
+                best = tmp;
+            }
+            while mul > k {
+                if let Some(el) = now.pop_front() {
+                    mul /= el;
+                }
+            }
+        } else {
+            flag = true;
+        }
+    }
+    if !flag {
+        println!("0");
+        return;
+    } 
+    let ans = if best.len() >= now.len() {
+        best.len()
+    } else {
+        now.len()
+    };
+    println!("{}", ans);
 }
