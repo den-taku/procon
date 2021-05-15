1b61525b539514a8d699f2aa2ce801c021dec300
DenTaku <dentaku1412@gmail.com>
2021-05-15 12:29:02 +0900

    AC ARC022B

--- atcoder/arc/arc022/src/bin/b.rs
+++ atcoder/arc/arc022/src/bin/b.rs
@@ -1,10 +1,28 @@
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        a: [i64; n]
     }
-    unimplemented!()
+    let mut best = HashSet::<i64>::new();
+    let mut now = HashSet::<i64>::new();
+    for i in 0..n {
+        if !now.insert(a[i].clone()) {
+            if now.len() > best.len() {
+                best = now.clone();
+            }
+            while a[i - now.len()] != a[i] {
+                now.remove(&a[i - now.len()]);
+            }
+        }
+    }
+    let ans = if best.len() > now.len() {
+        best.len()
+    } else {
+        now.len()
+    };
+    println!("{}", ans);
 }
