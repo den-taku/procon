4ad99f8c3f8d94d8fc0f609804252285ca799e9e
DenTaku <dentaku1412@gmail.com>
2021-05-17 15:17:30 +0900

    AC ABC017D

--- atcoder/abc/abc017/src/bin/d.rs
+++ atcoder/abc/abc017/src/bin/d.rs
@@ -1,10 +1,33 @@
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        _m: usize,
+        f: [i64; n]
     }
-    unimplemented!()
+    let mut dp = vec![0i64; n];
+    let mut now = HashSet::new();
+    dp[0] = 1;
+    now.insert(f[0]);
+    let mut sum = 1;
+    for i in 1..n {
+        if !now.insert(f[i]) {
+            while f[i] != f[i - now.len()]  {
+                now.remove(&f[i - now.len()]);
+            }
+        }
+        if now.len() == i + 1 {
+            sum += dp[i-1];
+            dp[i] += sum;
+        } else {
+            for j in 0..now.len() {
+                dp[i] += dp[i - 1 - j];
+            }
+        }
+        dp[i] %= 1000000007;
+    }
+    println!("{}", dp.pop().unwrap());
 }
