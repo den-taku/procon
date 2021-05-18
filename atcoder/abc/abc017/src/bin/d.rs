007a86aae5df3cf09ffcd5cc4ceb746bb2f40922
DenTaku <dentaku1412@gmail.com>
2021-05-18 17:30:20 +0900

    Refuctoring

--- atcoder/abc/abc017/src/bin/d.rs
+++ atcoder/abc/abc017/src/bin/d.rs
@@ -8,6 +8,10 @@ fn main() {
         _m: usize,
         f: [i64; n]
     }
+    println!("{}", dynamic(n, f));
+}
+
+fn dynamic(n: usize, f: Vec<i64>) -> i64 {
     let mut dp = vec![0i64; n];
     let mut now = HashSet::new();
     dp[0] = 1;
@@ -15,12 +19,12 @@ fn main() {
     let mut sum = 1;
     for i in 1..n {
         if !now.insert(f[i]) {
-            while f[i] != f[i - now.len()]  {
+            while f[i] != f[i - now.len()] {
                 now.remove(&f[i - now.len()]);
             }
         }
         if now.len() == i + 1 {
-            sum += dp[i-1];
+            sum += dp[i - 1];
             dp[i] += sum;
         } else {
             for j in 0..now.len() {
@@ -29,5 +33,5 @@ fn main() {
         }
         dp[i] %= 1000000007;
     }
-    println!("{}", dp.pop().unwrap());
+    dp.pop().unwrap()
 }
