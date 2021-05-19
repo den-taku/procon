692c6de27fa2f83784ea36ade895a99ddbe5d65b
DenTaku <dentaku1412@gmail.com>
2021-05-19 22:35:56 +0900

    Refactoring

--- atcoder/abc/abc083/src/bin/d.rs
+++ atcoder/abc/abc083/src/bin/d.rs
@@ -5,12 +5,7 @@ fn main() {
     input! {
         s: String
     }
-    let s = trans(&s);
-    let mut sum = vec![0; s.len()+1];
-    sum[0] += 0;
-    for i in 1..s.len()+1 {
-        sum[i] = sum[i - 1] + s[i-1];
-    }
+    let sum = make_cumulative_sum(&s);
     if s.len() % 2 == 1 {
         for i in 0..(s.len() + 1) / 2 {
             let all = sum[s.len() - i] - sum[i];
@@ -33,27 +28,15 @@ fn main() {
     
 }
 
-fn trans(s: &str) -> Vec<i32> {
+fn make_cumulative_sum(s: &str) -> Vec<i32> {
     let mut v = Vec::new();
+    v.push(0);
     for c in s.chars() {
         match c {
-            '0' => v.push(0),
-            '1' => v.push(1),
-            _ => unreachable!(),
+            '0' => v.push(*v.last().unwrap()),
+            '1' => v.push(*v.last().unwrap() + 1),
+            _ => unreachable!()
         }
     }
     v
 }
-
-// fn make_cumulative_sum(s: &str) -> Vec<i32> {
-//     let mut v = Vec::new();
-//     v.push(0);
-//     for c in s.chars() {
-//         match c {
-//             '0' => v.push(*v.last().unwrap()),
-//             '1' => v.push(*v.last().unwrap() + 1),
-//             _ => unreachable!()
-//         }
-//     }
-//     v
-// }
