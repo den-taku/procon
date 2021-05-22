6fd652cb55721de54cf26465b9278a502c805c87
DenTaku <dentaku1412@gmail.com>
2021-05-23 07:28:45 +0900

    AC ABC202

--- atcoder/abc/abc202/src/bin/c.rs
+++ atcoder/abc/abc202/src/bin/c.rs
@@ -1,10 +1,21 @@
 use proconio::{fastout, input};
+use std::collections::BTreeSet;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n]
     }
-    unimplemented!()
+    let mut ans = 0;
+    let mut count = vec![0i64; n+1];
+    for i in 0..n {
+        count[b[c[i] - 1]] += 1;
+    }
+    for i in 0..n {
+        ans += count[a[i]];
+    }
+    println!("{}", ans);
 }
