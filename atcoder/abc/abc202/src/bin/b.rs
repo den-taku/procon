97458902ac065d9cb76f8c88a3c0282001d7cb68
DenTaku <dentaku1412@gmail.com>
2021-05-23 07:06:02 +0900

    AC ABC202B

--- atcoder/abc/abc202/src/bin/b.rs
+++ atcoder/abc/abc202/src/bin/b.rs
@@ -3,8 +3,20 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        s: String
     }
-    unimplemented!()
+    let mut t = "".to_string();
+    for e in s.chars() {
+        match e {
+            '0' => { t = t + "0" },
+            '1' => { t = t + "1" },
+            '6' => { t = t + "9" },
+            '8' => { t = t + "8" },
+            '9' => { t = t + "6" },
+            _ => unreachable!()
+        }
+    }
+    for e in t.chars().rev() {
+        print!("{}", e);
+    }
 }
