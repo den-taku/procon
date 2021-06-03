58c0fc5c3b9dd0593705e5b7a912d2aef612a2d8
DenTaku <dentaku1412@gmail.com>
2021-06-03 22:06:44 +0900

    Initialize for AOJ

---
+++ aoj/rmq/src/bin/a.rs
@@ -0,0 +1,11 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        com: [(usize, usize, usize);q]
+    }
+    unimplemented!()
+}
