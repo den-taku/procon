fd34f6d684db574b4e4ea26fcff4bcd4e3612d7e
DenTaku <dentaku1412@gmail.com>
2021-06-22 00:33:01 +0900

    AC ABC009 D

--- atcoder/abc/abc009/src/bin/d.rs
+++ atcoder/abc/abc009/src/bin/d.rs
@@ -5,54 +5,39 @@ fn main() {
     input! {
         k: usize,
         m: usize,
-        mut a: [u32; k],
-        c: [u32; k]
+        a: [u64; k],
+        mut c: [u64; k]
     }
-    if m < k {
+    if m <= k {
         println!("{}", a[m - 1])
     } else {
-        a.append(&mut vec![0; k - 1]);
-        (0..k - 1).fold((), |_, i| {
-            let mut ans = 0;
-            (0..k).fold((), |_, j| {
-                ans ^= c[j] & a[i + k - j - 1];
-            });
-            a[k + i] = ans;
+        let mut tmp = vec![0u64; k * k - k];
+        (0..k-1).fold((), |_, i| {
+            tmp[i * k + i] = 0xffffffff;
         });
-        // println!("{:?}", a);
-        if m < 2 * k {
-            println!("{}", a[m - 1]);
-        } else {
-            let mut tmp = vec![0u32; k * k - k];
-            for i in 0..k - 1 {
-                for j in 0..k {
-                    tmp[i * k + j] = if i == j { 1 } else { 0 };
-                }
-            }
-            let mut a: Vec<u32> = a.iter().skip(k - 1).map(|e| *e).collect();
-            a.append(&mut tmp);
-            let matrix = Matrix::new(a);
-            // println!("{:?}", matrix);
-            unimplemented!()
-        }
+        c.append(&mut tmp);
+        let index = m - k;
+        let a: Vec<u64> = a.iter().rev().map(|e| *e).collect();
+        println!("{}", (Matrix::new(c, k).pow(index) * a)[0]);
     }
 }
 
 #[derive(Debug, Clone)]
 struct Matrix {
-    array: Vec<u32>,
+    array: Vec<u64>,
     k: usize,
 }
 
 impl Matrix {
-    fn new(array: Vec<u32>) -> Self {
-        let k = array.len();
+    #[inline]
+    fn new(array: Vec<u64>, k: usize) -> Self {
         Self { array, k }
     }
 }
 
 impl std::ops::Index<usize> for Matrix {
-    type Output = u32;
+    type Output = u64;
+    #[inline]
     fn index(&self, index: usize) -> &Self::Output {
         &self.array[index]
     }
@@ -60,6 +45,7 @@ impl std::ops::Index<usize> for Matrix {
 
 impl std::ops::Mul for Matrix {
     type Output = Self;
+    #[inline]
     fn mul(self, rhs: Self) -> Self::Output {
         let k = self.k;
         let mut array = vec![0; k * k];
@@ -68,8 +54,38 @@ impl std::ops::Mul for Matrix {
                 array[i * k + j] = (0..k).fold(0, |e, l| e ^ (self[i * k + l] & rhs[j + l * k]))
             })
         });
-        Self::new(array)
+        Self::new(array, k)
     }
 }
 
-// TODO : Implement 繰り返し二乗法
+impl std::ops::Mul<Vec<u64>> for Matrix {
+    type Output = Vec<u64>;
+    #[inline]
+    fn mul(self, rhs: Vec<u64>) -> Self::Output {
+        let k = self.k;
+        let mut vec = vec![0; k];
+        (0..k).fold((), |_, i| {
+            vec[i] = (0..k).fold(0, |s, j| s ^ (self[i * k + j] & rhs[j]));
+        });
+        vec
+    }
+}
+
+impl Matrix {
+    #[inline]
+    fn pow(&self, n: usize) -> Self {
+        if n == 0 {
+            let mut e = vec![0; self.k * self.k];
+            (0..self.k).fold((), |_, i| {
+                e[i * self.k + i] = 1;
+            });
+            Self::new(e, self.k)
+        } else if n == 1 {
+            self.clone()
+        } else if n % 2 == 0 {
+            (self.clone() * self.clone()).pow(n / 2)
+        } else {
+            (self.clone() * self.clone()).pow(n / 2) * self.clone()
+        }
+    }
+}
