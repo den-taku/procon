12d4ac656743295b71b0f5fb4857b00b96795b9e
den-taku <dentaku1412@gmail.com>
2021-07-08 13:36:46 +0900

    Initialize for Hungarian with DFS

---
+++ library/src/hungarian.rs
@@ -0,0 +1,14 @@
+#![allow(dead_code)]
+
+pub mod hungarian_library {
+
+    #[cfg(test)]
+    mod tests_hungarian {
+        use super::*;
+
+        #[test]
+        fn for_hungarian() {
+            assert_eq!(1 + 2, 3);
+        }
+    }
+}
--- library/src/lib.rs
+++ library/src/lib.rs
@@ -2,5 +2,6 @@ pub mod bit_sum;
 pub mod dinic;
 pub mod enumeration;
 pub mod ford_fulkerson;
+pub mod hungarian;
 pub mod range_minimum_query;
 pub mod red_black_tree;
