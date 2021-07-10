848fb8903e763b618bcb758f493254236fc9cc91
den-taku <dentaku1412@gmail.com>
2021-07-11 00:02:33 +0900

    AC Pa-2019

--- aoj/combine_2_elements/src/bin/d.rs
+++ aoj/combine_2_elements/src/bin/d.rs
@@ -1,32 +1,22 @@
-//use proconio::{fastout, input};
-use std::fmt::Debug;
-use std::str::FromStr;
+use proconio::{fastout, input};
 
-// #[fastout]
+#[fastout]
 fn main() {
-    // input! {
-    //     n: usize,
-    //     t: [[i128; 50];50]
-    // }
-    let n = read_line::<usize>()[0];
-    let mut t = Vec::with_capacity(n);
-    let _ = (0..n).map(|_| {
-        let elem = read_line::<i64>();
-        t.push(elem);
-    });
-    t = t;
-    unimplemented!()
-}
-
-fn read_line<T>() -> Vec<T>
-where
-    T: FromStr,
-    <T as FromStr>::Err: Debug,
-{
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    s.trim()
-        .split_whitespace()
-        .map(|c| T::from_str(c).unwrap())
-        .collect()
+  input! {
+    n: usize,
+    m: usize,
+    a: [i64; n * m]
+  }
+  let mut max = 0;
+  let mut est = 0;
+  for i in 0.. m {
+    for j in i..m {
+      for k in 0..n {
+        est += std::cmp::max(a[k * m + i], a[k * m + j]); 
+      }
+      max = std::cmp::max(max, est);
+      est = 0;
+    }
+  }
+  println!("{}", max);
 }
