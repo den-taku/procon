cb40920f5cc93fbffeba6c3963232509dcc82e6a
den-taku <dentaku1412@gmail.com>
2021-07-10 23:22:17 +0900

    AC ABC209 D

--- atcoder/abc/abc209/src/bin/d.rs
+++ atcoder/abc/abc209/src/bin/d.rs
@@ -1,3 +1,48 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+        ab: [(usize, usize); n - 1],
+        queries: [(usize, usize); q]
+    }
+    let mut vec = vec![vec![]; n];
+    for &(a, b) in &ab {
+        vec[a - 1].push(b);
+        vec[b - 1].push(a);
+    }
+    let dis = bfs(n, &vec);
+    for &(c, d) in &queries {
+        println!(
+            "{}",
+            if (dis[c - 1] + dis[d - 1]) % 2 == 0 {
+                "Town"
+            } else {
+                "Road"
+            }
+        )
+    }
+}
+
+#[inline]
+fn bfs(n: usize, graph: &Vec<Vec<usize>>) -> Vec<i32> {
+    let mut que = std::collections::VecDeque::<usize>::with_capacity(n);
+    let mut v = vec![0; n];
+    que.push_back(1);
+    while !que.is_empty() {
+        let next = que.pop_front().unwrap();
+        for &e in &graph[next - 1] {
+            if v[e - 1] == 0 {
+                v[e - 1] = v[next - 1] + 1;
+                que.push_back(e);
+            }
+        }
+    }
+    v
+}
+
 /* use proconio::{fastout, input};
 
 #[fastout]
@@ -8,7 +53,7 @@ fn main() {
         ab: [(usize, usize); n - 1],
         queries: [(usize, usize); q]
     }
-    // let mut set1 = 
+    // let mut set1 =
     let mut vec = vec![std::usize::MAX;n * n];
     for &(a, b) in &ab {
         //
@@ -16,7 +61,7 @@ fn main() {
     // for k in 0..n {
     //     for i in 0..n {
     //         for j in 0..n {
-                
+
     //         }
     //     }
     // }
@@ -152,7 +197,7 @@ struct UnionFind {
 impl UnionFind {
     fn new(n: usize) -> UnionFind {
         UnionFind {
-            par: { 
+            par: {
                 let mut v = Vec::with_capacity(n);
                 for i in 0..n {
                     v.push(i);
@@ -170,7 +215,7 @@ impl UnionFind {
             self.find(self.par[x])
         }
     }
-    
+
     fn unite(&mut self, x: usize, y: usize) {
         let x_par = self.find(x);
         let y_par = self.find(y);
