f5670efafcb52c4c768e61359da7ffd7e6fa4968
den-taku <dentaku1412@gmail.com>
2021-07-12 13:39:34 +0900

    Fix compete.toml

--- aoj/marukaite/src/bin/e.rs
+++ aoj/marukaite/src/bin/e.rs
@@ -8,11 +8,13 @@ fn main() {
     // }
     let n = read_line::<usize>()[0];
     let mut t = Vec::with_capacity(n);
-    let _ = (0..n).map(|_| {
-        let elem = read_line::<i64>();
-        t.push(elem);
-    }).collect::<Vec<()>>;
-    _t = t;
+    let _ = (0..n)
+        .map(|_| {
+            let elem = read_line::<i64>();
+            t.push(elem);
+        })
+        .collect::<Vec<()>>();
+    t = t;
     unimplemented!()
 }
 
