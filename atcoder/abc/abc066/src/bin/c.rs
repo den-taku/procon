1790527c62c67a9df7bb66c03a965208e0e66f18
den-taku <dentaku1412@gmail.com>
2021-07-20 16:25:02 +0900

    AC ABC066 C

--- atcoder/abc/abc066/src/bin/c.rs
+++ atcoder/abc/abc066/src/bin/c.rs
@@ -4,7 +4,25 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        a: [i32; n]
     }
-    unimplemented!()
+    let mut queue = std::collections::VecDeque::with_capacity(n);
+    for (i, &e) in a.iter().enumerate() {
+        if i % 2 == 0 {
+            queue.push_back(e)
+        } else {
+            queue.push_front(e)
+        }
+    }
+    if n % 2 == 0 {
+        for e in queue.iter().take(n - 1) {
+            print!("{} ", e);
+        }
+        println!("{}", queue[n - 1])
+    } else {
+        for &e in queue.iter().rev().take(n - 1) {
+            print!("{} ", e);
+        }
+        println!("{}", queue[0])
+    }
 }
