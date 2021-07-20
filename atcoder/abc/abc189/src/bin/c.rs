b600110c9c0674403bf56f0b653be3c39785eb7d
den-taku <dentaku1412@gmail.com>
2021-07-20 16:48:38 +0900

    AC ABC189 C

--- atcoder/abc/abc189/src/bin/c.rs
+++ atcoder/abc/abc189/src/bin/c.rs
@@ -1,10 +1,31 @@
 use proconio::{fastout, input};
+use std::cmp::max;
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        a: [i32; n]
     }
-    unimplemented!()
+    let set: HashSet<i32> = a.iter().cloned().collect();
+    let mut ans = 0;
+    for &e in set.iter() {
+        ans = max(ans, calc(e, &a))
+    }
+    println!("{}", ans);
+}
+
+fn calc(e: i32, a: &[i32]) -> i32 {
+    let mut better = 0i32;
+    let mut est = 0;
+    for &el in a {
+        if el >= e {
+            est += 1;
+        } else {
+            better = max(est, better);
+            est = 0;
+        }
+    }
+    max(est, better) * e
 }
