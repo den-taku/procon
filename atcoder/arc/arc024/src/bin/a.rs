b09ebbbadc9786be15d8b99dd4305759a497200c
den-taku <dentaku1412@gmail.com>
2021-07-21 19:39:29 +0900

    AC ARC024 A

--- atcoder/arc/arc024/src/bin/a.rs
+++ atcoder/arc/arc024/src/bin/a.rs
@@ -3,8 +3,21 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        l: usize,
+        r: usize,
+        ls: [i32; l],
+        rs: [i32; r]
     }
-    unimplemented!()
+    let mut count = 0;
+    let mut matched = vec![false; r];
+    for l in ls {
+        for (i, &r) in rs.iter().enumerate() {
+            if l == r && !matched[i] {
+                matched[i] = true;
+                count += 1;
+                break;
+            }
+        }
+    }
+    println!("{}", count);
 }
