c07ce124cbd299d218fd180dad367a44b04ed5b3
den-taku <dentaku1412@gmail.com>
2021-07-21 20:32:09 +0900

    AC ABC171 C

--- atcoder/abc/abc171/src/bin/c.rs
+++ atcoder/abc/abc171/src/bin/c.rs
@@ -1,10 +1,20 @@
 use proconio::{fastout, input};
 
+const A: u8 = 'a' as u8;
+
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        mut n: usize,
     }
-    unimplemented!()
+    let mut s = Vec::with_capacity(10);
+    while !(n == 0) {
+        n -= 1;
+        s.push(n % 26);
+        n /= 26;
+    }
+    for &c in s.iter().rev() {
+        print!("{}", (A + c as u8) as char)
+    }
+    println!("")
 }
