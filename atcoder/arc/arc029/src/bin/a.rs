3baeadbcd115b42275d0013b64822f334c581684
den-taku <dentaku1412@gmail.com>
2021-07-21 21:09:21 +0900

    AC ARC029 A

--- atcoder/arc/arc029/src/bin/a.rs
+++ atcoder/arc/arc029/src/bin/a.rs
@@ -1,10 +1,25 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
+use std::cmp::{max, min};
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        ts: [i32; n]
     }
-    unimplemented!()
+    if n <= 2 {
+        println!("{}", ts.iter().max().unwrap());
+    } else {
+        let sum = ts.iter().sum::<i32>();
+        let mut est = std::i32::MAX;
+        for i in 0..n {
+            est = min(est, max(sum - ts[i], ts[i]));
+        }
+        for v in (0..n).into_iter().permutations(2) {
+            let a = ts[v[0]] + ts[v[1]];
+            est = min(est, max(a, sum - a))
+        }
+        println!("{}", est);
+    }
 }
