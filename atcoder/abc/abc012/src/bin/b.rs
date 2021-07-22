3eb8bc5c79318e70577fad08af73561c6c49a593
den-taku <dentaku1412@gmail.com>
2021-07-22 15:13:42 +0900

    AC ABC012 B

--- atcoder/abc/abc012/src/bin/b.rs
+++ atcoder/abc/abc012/src/bin/b.rs
@@ -4,7 +4,15 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
     }
-    unimplemented!()
+    let h = n / 3600;
+    let h1 = h / 10;
+    let h2 = h % 10;
+    let m = (n - 3600 * h) / 60;
+    let m1 = m / 10;
+    let m2 = m % 10;
+    let s = n - 3600 * h - 60 * m;
+    let s1 = s / 10;
+    let s2 = s % 10;
+    println!("{}{}:{}{}:{}{}", h1, h2, m1, m2, s1, s2);
 }
