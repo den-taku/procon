3fb965be0f9c0a2a99f7d2d57ba4214f3a249335
den-taku <dentaku1412@gmail.com>
2021-07-22 18:00:29 +0900

    AC AGC041 A

--- atcoder/agc/agc041/src/bin/a.rs
+++ atcoder/agc/agc041/src/bin/a.rs
@@ -3,8 +3,17 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        n: u64,
+        a: u64,
+        b: u64
     }
-    unimplemented!()
+    if a >= b && (a - b) % 2 == 0 || a < b && (b - a) % 2 == 0 {
+        println!("{}", (std::cmp::max(a, b) - std::cmp::min(a, b)) / 2)
+    } else {
+        println!("{}", {
+            let res1 = std::cmp::min(a - 1, b - 1) + 1;
+            let resn = std::cmp::min(n - a, n - b) + 1;
+            (std::cmp::max(a, b) - std::cmp::min(a, b) - 1) / 2 + std::cmp::min(res1, resn)
+        })
+    }
 }
