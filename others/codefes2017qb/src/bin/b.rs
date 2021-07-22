1efa7102c2cfef43caf3e86df699e02c2ef7f28b
den-taku <dentaku1412@gmail.com>
2021-07-22 16:32:15 +0900

    AC CODE FESTIVAL 2017 B

--- others/codefes2017qb/src/bin/b.rs
+++ others/codefes2017qb/src/bin/b.rs
@@ -4,7 +4,33 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        dificulties: [u32; n],
+        m: usize,
+        needed: [u32; m]
     }
-    unimplemented!()
+    if n < m {
+        println!("NO")
+    } else {
+        let mut f = std::collections::HashMap::new();
+        for d in dificulties {
+            if let Some(&e) = f.get(&d) {
+                f.insert(d, e + 1);
+            } else {
+                f.insert(d, 1);
+            }
+        }
+        for t in needed {
+            if let Some(&e) = f.get(&t) {
+                if e == 1 {
+                    f.remove(&t);
+                } else {
+                    f.insert(t, e - 1);
+                }
+            } else {
+                println!("NO");
+                return;
+            }
+        }
+        println!("YES");
+    }
 }
