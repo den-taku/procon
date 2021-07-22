0227fa7cbd72bb7d27c115d26c4f9fc80cf75855
den-taku <dentaku1412@gmail.com>
2021-07-22 16:11:49 +0900

    AC ABC182 C

--- atcoder/abc/abc182/src/bin/c.rs
+++ atcoder/abc/abc182/src/bin/c.rs
@@ -3,8 +3,67 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        mut n: usize,
     }
-    unimplemented!()
+    let mut c1 = 0i32;
+    let mut c2 = 0;
+    let mut c3 = 0;
+    let mut sum = 0;
+    while !(n == 0) {
+        match (n % 10) % 3 {
+            0 => {
+                c3 += 1;
+            }
+            1 => {
+                c1 += 1;
+                sum += 1;
+            }
+            2 => {
+                c2 += 1;
+                sum += 2;
+            }
+            _ => unreachable!(),
+        }
+        n /= 10;
+    }
+    match sum % 3 {
+        0 => {
+            println!("0");
+        }
+        1 => {
+            if c1 > 0 {
+                if c1 == 1 && c2 == 0 && c3 == 0 {
+                    println!("-1")
+                } else {
+                    println!("1")
+                }
+            } else if c2 > 1 {
+                if c1 == 0 && c2 == 2 && c3 == 0 {
+                    println!("-1")
+                } else {
+                    println!("2")
+                }
+            } else {
+                println!("-1")
+            }
+        }
+        2 => {
+            if c2 > 0 {
+                if c1 == 0 && c2 == 1 && c3 == 0 {
+                    println!("-1")
+                } else {
+                    println!("1")
+                }
+            } else if c1 > 1 {
+                if c1 == 2 && c2 == 0 && c3 == 0 {
+                    println!("-1")
+                } else {
+                    println!("2")
+                }
+            } else {
+                println!("-1")
+            }
+        }
+        _ => unreachable!(),
+    }
 }
