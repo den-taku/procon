a76ca30f2433d56f5547aa0e9b359931c1c081d1
den-taku <dentaku1412@gmail.com>
2021-07-23 16:59:26 +0900

    AC ABC033 B

--- atcoder/abc/abc033/src/bin/b.rs
+++ atcoder/abc/abc033/src/bin/b.rs
@@ -4,7 +4,22 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        towns: [(String, i32); n]
     }
-    unimplemented!()
+    let mut sum = 0;
+    let mut max = 0;
+    for (i, (_, num)) in towns.iter().cloned().enumerate() {
+        if towns[max].1 < num {
+            max = i;
+        }
+        sum += num;
+    }
+    println!(
+        "{}",
+        if sum / 2 < towns[max].1 {
+            towns[max].0.clone()
+        } else {
+            "atcoder".to_string()
+        }
+    )
 }
