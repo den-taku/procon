3310da269241ecd8dd2d260e77723c191f4459ad
den-taku <dentaku1412@gmail.com>
2021-07-23 17:07:19 +0900

    AC ARC056 A

--- atcoder/arc/arc056/src/bin/a.rs
+++ atcoder/arc/arc056/src/bin/a.rs
@@ -1,10 +1,16 @@
 use proconio::{fastout, input};
+use std::cmp::min;
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        a: u64,
+        b: u64,
+        k: u64,
+        l: u64
     }
-    unimplemented!()
+    let all1 = a * k;
+    let allk = (k / l) * b + if k % l != 0 { b } else { 0 };
+    let k1 = (k / l) * b + (k % l) * a;
+    println!("{}", min(all1, min(allk, k1)))
 }
