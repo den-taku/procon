835450e8e7620ed4c8117a3f4f3422877f928df5
den-taku <dentaku1412@gmail.com>
2021-07-23 17:23:08 +0900

    AC ABC029 C

--- atcoder/abc/abc029/src/bin/c.rs
+++ atcoder/abc/abc029/src/bin/c.rs
@@ -4,7 +4,17 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
     }
-    unimplemented!()
+    let mut stack = Vec::new();
+    stack.push("".to_string());
+    while !stack.is_empty() {
+        let string = stack.pop().unwrap();
+        if string.len() == n {
+            println!("{}", string);
+        } else {
+            stack.push(format!("{}c", string.clone()));
+            stack.push(format!("{}b", string.clone()));
+            stack.push(format!("{}a", string));
+        }
+    }
 }
