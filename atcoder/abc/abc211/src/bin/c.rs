cc7f5758c8fea2b855e440c8c7860504aace60d6
den-taku <dentaku1412@gmail.com>
2021-07-25 00:34:18 +0900

    AC ABC211 C

--- atcoder/abc/abc211/src/bin/c.rs
+++ atcoder/abc/abc211/src/bin/c.rs
@@ -7,55 +7,53 @@ fn main() {
     input! {
         s: String
     }
-    let mut dp = vec![vec![(0, 0); 8]; s.len() + 1];
+    let mut dp = vec![vec![0; 9]; s.len() + 1];
+    dp[0][0] = 1;
     let mut i = 1;
     for c in s.chars() {
         match c {
             'c' => {
-                update(&mut dp, i, 0);
-            }
-            'h' => {
                 update(&mut dp, i, 1);
             }
-            'o' => {
+            'h' => {
                 update(&mut dp, i, 2);
             }
-            'k' => {
+            'o' => {
                 update(&mut dp, i, 3);
             }
-            'u' => {
+            'k' => {
                 update(&mut dp, i, 4);
             }
-            'd' => {
+            'u' => {
                 update(&mut dp, i, 5);
             }
-            'a' => {
+            'd' => {
                 update(&mut dp, i, 6);
             }
-            'i' => {
+            'a' => {
                 update(&mut dp, i, 7);
             }
+            'i' => {
+                update(&mut dp, i, 8);
+            }
             _ => {
-                for j in 0..8 {
+                for j in 0..9 {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
         }
         i += 1;
     }
-    println!("{}", dp[s.len()][7]);
+    println!("{}", dp[s.len()][8]);
 }
 
 #[inline]
-fn update(dp: &mut Vec<Vec<(u64, u64)>>, index: usize, kind: usize) {
-    dp[index][kind].0 = (dp[index - 1][kind] + 1) % MOD;
-    for j in 0..8 {
+fn update(dp: &mut Vec<Vec<u64>>, index: usize, kind: usize) {
+    dp[index][kind] = (dp[index - 1][kind - 1] + dp[index - 1][kind]) % MOD;
+    for j in 0..9 {
         if j == kind {
             continue;
         }
         dp[index][j] = dp[index - 1][j];
-    }
-    for j in 1..8 {
-        dp[index][j].1 = dp[index][j - 1].1 * dp[index][j].0;
     }
 }
