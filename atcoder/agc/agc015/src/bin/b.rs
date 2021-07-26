b1ac3e0e932fceae1f388adaca6dda0c5c7b8a23
den-taku <dentaku1412@gmail.com>
2021-07-26 19:10:40 +0900

    AC AGC015 B

--- atcoder/agc/agc015/src/bin/b.rs
+++ atcoder/agc/agc015/src/bin/b.rs
@@ -3,8 +3,17 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        s: String
     }
-    unimplemented!()
+    let mut ans = 0;
+    for (i, c) in s.chars().enumerate() {
+        if c == 'U' {
+            ans += s.len() - i - 1;
+            ans += 2 * i;
+        } else {
+            ans += 2 * (s.len() - i - 1);
+            ans += i;
+        }
+    }
+    println!("{}", ans);
 }
