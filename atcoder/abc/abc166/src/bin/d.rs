6ff034ddb954dcb032b6a35ddfa62f733fbe7d45
den-taku <dentaku1412@gmail.com>
2021-07-26 19:41:42 +0900

    AC ABC166 D

--- atcoder/abc/abc166/src/bin/d.rs
+++ atcoder/abc/abc166/src/bin/d.rs
@@ -3,8 +3,37 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        x: u64
     }
-    unimplemented!()
+    let mut memo = std::collections::HashMap::new();
+    let mut ans1 = 0;
+    let mut ans2 = 0;
+    for i in 0..1000 {
+        for j in 0..i + 1 {
+            let v1 = if let Some(&e) = memo.get(&i) {
+                e
+            } else {
+                let tmp = (i as u64).pow(5u32);
+                memo.insert(i, tmp);
+                tmp
+            };
+            let v2 = if let Some(&e) = memo.get(&j) {
+                e
+            } else {
+                let tmp = (j as u64).pow(5u32);
+                memo.insert(j, tmp);
+                tmp
+            };
+            if v1 - v2 == x {
+                ans1 = i;
+                ans2 = j as i128;
+                break;
+            } else if v1 + v2 == x {
+                ans1 = i;
+                ans2 = -(j as i128);
+                break;
+            }
+        }
+    }
+    println!("{} {}", ans1, ans2);
 }
