eef31152b75c85d0a78509442c7a2832e2b1e4c2
den-taku <dentaku1412@gmail.com>
2021-07-26 17:45:02 +0900

    AC DISCO2020 B

--- others/disco2020/src/bin/b.rs
+++ others/disco2020/src/bin/b.rs
@@ -4,7 +4,23 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        a: [i64; n]
     }
-    unimplemented!()
+    let sum: i64 = a.iter().sum();
+    let mut count = 0;
+    let mut index = 0;
+    for (i, &e) in a.iter().enumerate() {
+        if 2 * (count + e) >= sum {
+            index = i;
+            break;
+        }
+        count += e;
+    }
+    let mut res = sum - count - a[index];
+    if count >= res {
+        res += a[index];
+    } else {
+        count += a[index]
+    }
+    println!("{}", (res - count).abs());
 }
