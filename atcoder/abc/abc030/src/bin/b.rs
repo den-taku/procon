4414866df8b38d617746fbdbe60c112565e1d65d
den-taku <dentaku1412@gmail.com>
2021-07-28 17:50:32 +0900

    AC ABC030 B

--- atcoder/abc/abc030/src/bin/b.rs
+++ atcoder/abc/abc030/src/bin/b.rs
@@ -4,8 +4,30 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: f64,
+        m: f64,
     }
-    unimplemented!()
+    let minite = 6.0 * m;
+    let hour = (30.0 * n + 30.0 * m / 60.0) % 360.0;
+    let dis = max(minite, hour) - min(minite, hour);
+    println!("{}", min(
+        dis,
+        360.0 - dis
+    ))
+}
+
+fn max(a: f64, b: f64) -> f64 {
+    if a > b {
+        a
+    } else {
+        b
+    }
+}
+
+fn min(a: f64, b: f64) -> f64 {
+    if a > b {
+        b
+    } else {
+        a
+    }
 }
