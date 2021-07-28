b99957fae59e5ce5a0256ca2221c8f757bd22971
den-taku <dentaku1412@gmail.com>
2021-07-28 18:18:37 +0900

    AC MUJIN B

--- others/mujin/src/bin/b.rs
+++ others/mujin/src/bin/b.rs
@@ -1,10 +1,40 @@
 use proconio::{fastout, input};
+use std::f32::consts::PI;
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        l1: f32,
+        l2: f32,
+        l3: f32,
     }
-    unimplemented!()
+    println!(
+        "{}",
+        PI * ((l1 + l2 + l3) * (l1 + l2 + l3)
+            - max(
+                max(
+                    wall(l1 - l2 - l3) * wall(l1 - l2 - l3),
+                    wall(l3 - l2 - l1) * wall(l3 - l2 - l1)
+                ),
+                wall(l2 - l3 - l1) * wall(l2 - l3 - l1)
+            ))
+    )
+}
+
+#[inline]
+fn wall(a: f32) -> f32 {
+    if a > 0.0 {
+        a
+    } else {
+        0.0
+    }
+}
+
+#[inline]
+fn max(a: f32, b: f32) -> f32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
