daa9b097b1a7ff735b1a8563777f177084413f23
den-taku <dentaku1412@gmail.com>
2021-07-29 19:33:04 +0900

    AC ABC064 D

--- atcoder/abc/abc064/src/bin/d.rs
+++ atcoder/abc/abc064/src/bin/d.rs
@@ -1,11 +1,32 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
     input! {
         _n: usize,
-        _t: [[i128; 50];50]
+        s: String
     }
-    unimplemented!()
+    let mut ans = VecDeque::new();
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        if c == '(' {
+            stack.push(true);
+            ans.push_back('(');
+        } else if stack.is_empty() {
+            ans.push_front('(');
+            ans.push_back(')');
+        } else {
+            stack.pop();
+            ans.push_back(')');
+        }
+    }
+    for _ in stack {
+        ans.push_back(')');
+    }
+    for e in ans {
+        print!("{}", e);
+    }
+    println!();
 }
