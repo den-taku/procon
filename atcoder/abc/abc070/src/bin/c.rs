ed00df18077bfe22cbf0d593b8abddf824e801c3
den-taku <dentaku1412@gmail.com>
2021-07-29 19:20:13 +0900

    AC ABC070 C

--- atcoder/abc/abc070/src/bin/c.rs
+++ atcoder/abc/abc070/src/bin/c.rs
@@ -4,8 +4,32 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        mut t: [u128; n]
     }
-    unimplemented!()
+    t.sort();
+    t.dedup();
+    let mut ans = 1;
+    for e in t {
+        ans = lcm(ans, e);
+    }
+    println!("{}", ans);
+}
+
+#[inline]
+fn gcd(a: u128, b: u128) -> u128 {
+    let mut max = std::cmp::max(a, b);
+    let mut min = std::cmp::min(a, b);
+    while min != 0 {
+        let tmp = min;
+        min = max % min;
+        max = tmp;
+    }
+    max
+}
+
+#[inline]
+fn lcm(a: u128, b: u128) -> u128 {
+    let gcd = gcd(a, b);
+    a / gcd * b
 }
