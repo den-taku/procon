ec3ffbdd250037223725342ec6b58f915478458f
den-taku <dentaku1412@gmail.com>
2021-07-31 09:35:08 +0900

    AC ARC110 B

--- atcoder/arc/arc110/src/bin/b.rs
+++ atcoder/arc/arc110/src/bin/b.rs
@@ -5,7 +5,56 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         _n: usize,
-        _t: [[i128; 50];50]
+        s: String
     }
-    unimplemented!()
+    if s == "1" {
+        println!("{}", 10u64.pow(10u32) * 2);
+        return;
+    } else if s == "0" {
+        println!("{}", 10u64.pow(10u32));
+        return;
+    } else if s.starts_with("11") {
+        let mut count = 0;
+        for (c, (x, i)) in s.chars().zip([('1', 0), ('1', 1), ('0', 2)].iter().cycle()) {
+            if c != *x {
+                count = 0;
+                break;
+            } else if i == &0 {
+                count += 1;
+            }
+        }
+        if count > 0 {
+            println!("{}", 10u64.pow(10u32) - count + 1);
+            return;
+        }
+    } else if s.starts_with("10") {
+        let mut count = 1;
+        for (c, (i, x)) in s.chars().zip([('1', 1), ('0', 2), ('1', 0)].iter().cycle()) {
+            if c != *i {
+                count = 0;
+                break;
+            } else if x == &0 {
+                count += 1;
+            }
+        }
+        if count > 0 {
+            println!("{}", 10u64.pow(10u32) - count + 1);
+            return;
+        }
+    } else if s.starts_with("01") {
+        let mut count = 1;
+        for (c, (i, x)) in s.chars().zip([('0', 2), ('1', 0), ('1', 1)].iter().cycle()) {
+            if c != *i {
+                count = 0;
+                break;
+            } else if x == &0 {
+                count += 1;
+            }
+        }
+        if count > 0 {
+            println!("{}", 10u64.pow(10u32) - count + 1);
+            return;
+        }
+    }
+    println!("0");
 }
