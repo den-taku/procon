5ba354cb16ac43ed105c860f930ef2df38f6a39c
den-taku <dentaku1412@gmail.com>
2021-08-02 11:26:40 +0900

    AC ABC169 D

--- atcoder/abc/abc169/src/bin/d.rs
+++ atcoder/abc/abc169/src/bin/d.rs
@@ -4,7 +4,41 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
     }
-    unimplemented!()
+    let map = prime_factor(n);
+    let mut ans = 0;
+    for (&_, &e) in map.iter() {
+        ans += 1;
+        let mut i = 2;
+        let mut e = e;
+        while e > i {
+            ans += 1;
+            e -= i;
+            i += 1;
+        }
+    }
+    println!("{}", ans);
+}
+
+#[inline]
+fn prime_factor(mut n: usize) -> std::collections::HashMap<usize, usize> {
+    let mut map = std::collections::HashMap::new();
+    let mut i = 2;
+    while i * i <= n {
+        while n % i == 0 {
+            if let Some(&e) = map.get(&i) {
+                map.insert(i, e + 1);
+            } else {
+                map.insert(i, 1);
+            }
+            n /= i;
+        }
+        i += 1;
+    }
+    if n != 1 {
+        map.insert(n, 1);
+        map
+    } else {
+        map
+    }
 }
