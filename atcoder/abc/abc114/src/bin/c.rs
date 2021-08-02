faf855496b4139d4163584e3f86897b5d9ec5fbc
den-taku <dentaku1412@gmail.com>
2021-08-02 12:59:13 +0900

    AC ABC114 C

--- atcoder/abc/abc114/src/bin/c.rs
+++ atcoder/abc/abc114/src/bin/c.rs
@@ -4,8 +4,35 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
     }
-    unimplemented!()
+    let v = [0, 3, 5, 7];
+    let mut ans = 0;
+    for mut i in 27.. {
+        let mut value = 0;
+        let mut digit = 1;
+        while i > 0 {
+            value += digit * v[i % 4];
+            i /= 4;
+            digit *= 10;
+        }
+        if n >= value && check(value) {
+            ans += 1;
+        } else if n < value {
+            break;
+        }
+    }
+    println!("{}", ans);
+}
+
+#[inline]
+fn check(mut v: usize) -> bool {
+    let mut u = Vec::new();
+    while v > 0 {
+        u.push(v % 10);
+        v /= 10;
+    }
+    u.sort();
+    u.dedup();
+    u.len() == 3 && u[0] != 0
 }
