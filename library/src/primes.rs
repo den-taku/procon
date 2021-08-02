1c89fed820f3b7c6e33432d39a8e0635c395abcd
den-taku <dentaku1412@gmail.com>
2021-08-03 01:42:08 +0900

    Omit implementation with macro

--- library/src/primes.rs
+++ library/src/primes.rs
@@ -1,6 +1,18 @@
 #![allow(dead_code)]
 
 pub mod primes_library {
+    macro_rules! impl_num {
+        ( $($e:ty),* ) => {
+            $(impl Prime<$e> {
+                impl_new!($e);
+            }
+
+            impl Iterator for Prime<$e> {
+                impl_next!($e);
+            })*
+        };
+    }
+
     macro_rules! impl_new {
         ($e:ty) => {
             pub fn new() -> Self {
@@ -38,69 +50,7 @@ pub mod primes_library {
         }
     }
 
-    impl Prime<isize> {
-        impl_new!(isize);
-    }
-
-    impl Iterator for Prime<isize> {
-        impl_next!(isize);
-    }
-
-    impl Prime<i32> {
-        impl_new!(i32);
-    }
-
-    impl Iterator for Prime<i32> {
-        impl_next!(i32);
-    }
-
-    impl Prime<i64> {
-        impl_new!(i64);
-    }
-
-    impl Iterator for Prime<i64> {
-        impl_next!(i64);
-    }
-
-    impl Prime<i128> {
-        impl_new!(i128);
-    }
-
-    impl Iterator for Prime<i128> {
-        impl_next!(i128);
-    }
-
-    impl Prime<usize> {
-        impl_new!(usize);
-    }
-
-    impl Iterator for Prime<usize> {
-        impl_next!(usize);
-    }
-
-    impl Prime<u32> {
-        impl_new!(u32);
-    }
-
-    impl Iterator for Prime<u32> {
-        impl_next!(u32);
-    }
-
-    impl Prime<u64> {
-        impl_new!(u64);
-    }
-
-    impl Iterator for Prime<u64> {
-        impl_next!(u64);
-    }
-
-    impl Prime<u128> {
-        impl_new!(u128);
-    }
-
-    impl Iterator for Prime<u128> {
-        impl_next!(u128);
-    }
+    impl_num!(isize, i32, i64, i128, usize, u32, u64, u128);
 
     #[cfg(test)]
     mod tests_primes {
