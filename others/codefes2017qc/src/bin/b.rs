a04bca458bd93047d749c34e66cfba90d5d8e28f
den-taku <dentaku1412@gmail.com>
2021-08-07 19:59:14 +0900

    AC B

--- others/codefes2017qc/src/bin/b.rs
+++ others/codefes2017qc/src/bin/b.rs
@@ -4,8 +4,25 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        a: [i64; n]
     }
-    unimplemented!()
+    println!("{}", rec(a));
+}
+
+#[inline]
+fn rec(mut a: Vec<i64>) -> u64 {
+    if let Some(v) = a.pop() {
+        if v % 2 == 0 {
+            let se = 3u64.pow(a.len() as u32);
+            let m1 = rec(a);
+            m1 * 2 + se
+        } else {
+            let m1 = 3u64.pow(a.len() as u32);
+            let se = rec(a);
+            m1 * 2 + se
+        }
+    } else {
+        0
+    }
 }
