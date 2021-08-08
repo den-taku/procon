7f76074e48f750b029ca9cb927731a112b5fc472
dentaku <dentaku1412@gmail.com>
2021-08-09 02:29:42 +0900

    AC ABC213 E

--- atcoder/abc/abc213/src/bin/e.rs
+++ atcoder/abc/abc213/src/bin/e.rs
@@ -1,11 +1,78 @@
 #![allow(unreachable_code)]
-use proconio::{fastout, input};
+use proconio::input;
+use std::collections::VecDeque;
 
-#[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        h: isize,
+        w: isize,
+        matrix: [String; h]
     }
-    unimplemented!()
+    let mut visited = vec![false; (h * w) as usize];
+    let load = convert(&matrix);
+    let mut queue1 = VecDeque::new();
+    let mut queue2 = VecDeque::new();
+    queue1.push_back((0, 0));
+    let mut ans = 0;
+    let neiberh = [-1, 0, 1, 0];
+    let neiberw = [0, 1, 0, -1];
+    let jumph = [-2, -2, -2, -1, -1, 0, 1, 1, 2, 2, 2, 1, 1, 0, -1, -1];
+    let jumpw = [-1, 0, 1, 1, 2, 2, 2, 1, 1, 0, -1, -1, -2, -2, -2, -1];
+    'out: loop {
+        while let Some((a, b)) = queue1.pop_front() {
+            if visited[(a * w + b) as usize] {
+                continue;
+            }
+            if (a, b) == (h - 1, w - 1) {
+                break 'out;
+            }
+            visited[(a * w + b) as usize] = true;
+            for (i, j) in neiberh.iter().zip(&neiberw) {
+                let newh = a + i;
+                let neww = b + j;
+                if newh >= 0
+                    && newh < h
+                    && neww >= 0
+                    && neww < w
+                    && !visited[(newh * w + neww) as usize]
+                {
+                    if load[(newh * w + neww) as usize] {
+                        queue1.push_back((newh, neww));
+                    } else {
+                        queue2.push_back((newh, neww));
+                    }
+                }
+            }
+            for (i, j) in jumph.iter().zip(&jumpw) {
+                let newh = a + i;
+                let neww = b + j;
+                if newh >= 0
+                    && newh < h
+                    && neww >= 0
+                    && neww < w
+                    && !visited[(newh * w + neww) as usize]
+                {
+                    queue2.push_back((newh, neww));
+                }
+            }
+        }
+        queue1 = queue2;
+        queue2 = VecDeque::new();
+        ans += 1;
+    }
+    println!("{}", ans);
+}
+
+#[inline]
+fn convert(matrix: &[String]) -> Vec<bool> {
+    let mut v = Vec::with_capacity(matrix.len() * matrix[0].len());
+    for s in matrix {
+        for c in s.chars() {
+            match c {
+                '.' => v.push(true),
+                _ => v.push(false),
+            }
+        }
+    }
+    v
 }
