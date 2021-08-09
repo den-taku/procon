16eb5184a0b4516733108eaa83c686e1155139a4
dentaku <dentaku1412@gmail.com>
2021-08-09 16:51:09 +0900

    AC ABC104 C

--- atcoder/abc/abc104/src/bin/c.rs
+++ atcoder/abc/abc104/src/bin/c.rs
@@ -1,11 +1,42 @@
 #![allow(unreachable_code)]
-use proconio::{fastout, input};
+use proconio::input;
+use std::cmp::min;
 
-#[fastout]
+// #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        d: usize,
+        g: usize,
+        pc: [(usize, usize); d]
     }
-    unimplemented!()
+    let mut ans = std::usize::MAX;
+    let pc = pc.into_iter().map(|e| (e.0, e.1 / 100)).collect::<Vec<_>>();
+    let g = g / 100;
+    'out: for i in 0..1 << d {
+        let mut sum = 0;
+        let mut count = 0;
+        for j in 0..d {
+            if (i >> j) & 1 == 1 {
+                sum += pc[j].0 * (j + 1) + pc[j].1;
+                count += pc[j].0;
+            }
+        }
+        if sum >= g {
+            ans = min(ans, count);
+            continue;
+        }
+        for j in (0..d).rev() {
+            if i >> j & 1 == 0 {
+                for _ in 0..(pc[j].0 as usize) {
+                    sum += j + 1;
+                    count += 1;
+                    if sum >= g {
+                        ans = min(ans, count);
+                        continue 'out;
+                    }
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
