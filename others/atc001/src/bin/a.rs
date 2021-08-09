63e1d1d00d241c8c2a1af5f263cc294c998715a7
dentaku <dentaku1412@gmail.com>
2021-08-10 01:41:50 +0900

    AC ATC001 A

--- others/atc001/src/bin/a.rs
+++ others/atc001/src/bin/a.rs
@@ -4,8 +4,60 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        h: isize,
+        w: isize,
+        s_vec: [String; h]
     }
-    unimplemented!()
+    let ((si, sj), (gi, gj), mut visited) = convert(s_vec);
+    let mut stack = Vec::new();
+    stack.push((si, sj));
+    visited[si * (w as usize) + sj] = true;
+    let dh = [-1, 0, 1, 0];
+    let dw = [0, 1, 0, -1];
+    while let Some((i, j)) = stack.pop() {
+        if gi == i && gj == j {
+            println!("Yes");
+            return;
+        }
+        for (dh, dw) in dh.iter().zip(dw.iter()) {
+            let newh = i as isize + dh;
+            let neww = j as isize + dw;
+            if newh >= 0
+                && newh < h
+                && neww >= 0
+                && neww < w
+                && !visited[(newh * w + neww) as usize]
+            {
+                stack.push((newh as usize, neww as usize));
+                visited[(newh * w + neww) as usize] = true;
+            }
+        }
+    }
+    println!("No");
+}
+
+#[inline]
+fn convert(s_vec: Vec<String>) -> ((usize, usize), (usize, usize), Vec<bool>) {
+    let mut v = Vec::with_capacity(s_vec.len());
+    let (mut si, mut sj) = (0, 0);
+    let (mut gi, mut gj) = (0, 0);
+    for (i, s) in s_vec.iter().cloned().enumerate() {
+        for (j, c) in s.chars().enumerate() {
+            match c {
+                's' => {
+                    v.push(false);
+                    si = i;
+                    sj = j;
+                }
+                'g' => {
+                    v.push(false);
+                    gi = i;
+                    gj = j;
+                }
+                '.' => v.push(false),
+                _ => v.push(true),
+            }
+        }
+    }
+    ((si, sj), (gi, gj), v)
 }
