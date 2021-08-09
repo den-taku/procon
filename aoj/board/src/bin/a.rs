a2f1bf783ec1b56af59c67a0ac55e203528c7e9a
dentaku <dentaku1412@gmail.com>
2021-08-09 15:10:45 +0900

    AC AOJ Board

--- aoj/board/src/bin/a.rs
+++ aoj/board/src/bin/a.rs
@@ -1,20 +1,85 @@
+// Board(https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2903)
+#![allow(dead_code)]
 #![allow(unreachable_code)]
-//use proconio::{fastout, input};
 
-// #[fastout]
 fn main() {
-    // input! {
-    //     n: usize,
-    //     t: [[i128; 50];50]
-    // }
-    let n = read_line::<usize>()[0];
-    let mut t = Vec::with_capacity(n);
-    let _ = (0..n).map(|_| {
-        let elem = read_line::<i64>();
-        t.push(elem);
-    }).collect::<Vec<()>>();
-    let _t = t;
-    unimplemented!()
+    let (r, c, matrix) = input();
+    let (map, count) = convert(matrix);
+    let mut id = 0;
+    let index = map
+        .iter()
+        .map(|e| {
+            if *e {
+                id += 1;
+                Some(id)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let mut adjacent = 0;
+    let mut edges = Vec::new();
+    let s = 0;
+    let t = count + 1;
+    let mut new = t + 1;
+    for i in 0..r {
+        for j in 0..c {
+            if map[i * c + j] {
+                if i + 1 < r && map[(i + 1) * c + j] {
+                    adjacent += 1;
+                    edges.push((new, t, 1));
+                    edges.push((index[i * c + j].unwrap(), new, std::i64::MAX));
+                    edges.push((index[(i + 1) * c + j].unwrap(), new, std::i64::MAX));
+                    new += 1;
+                }
+                if j + 1 < c && map[i * c + j + 1] {
+                    adjacent += 1;
+                    edges.push((s, new, 1));
+                    edges.push((new, index[i * c + j].unwrap(), std::i64::MAX));
+                    edges.push((new, index[i * c + j + 1].unwrap(), std::i64::MAX));
+                    new += 1;
+                }
+            }
+        }
+    }
+    println!(
+        "{}",
+        dinic_library::Dinic::new(new, &edges).max_flow(s, t) + count as i64 - adjacent as i64
+    );
+}
+
+#[inline]
+fn convert(matrix: Vec<String>) -> (Vec<bool>, usize) {
+    let mut v = Vec::with_capacity(matrix.len() * matrix[0].len());
+    let mut count = 0;
+    for m in matrix {
+        for c in m.chars() {
+            match c {
+                '#' => {
+                    v.push(true);
+                    count += 1
+                }
+                '\n' => {}
+                _ => v.push(false),
+            }
+        }
+    }
+    (v, count)
+}
+
+#[inline]
+fn input() -> (usize, usize, Vec<String>) {
+    let (r, c) = {
+        let elm = read_line::<usize>();
+        (elm[0], elm[1])
+    };
+    let mut v = Vec::with_capacity(r);
+    for _ in 0..r {
+        let mut s = String::new();
+        std::io::stdin().read_line(&mut s).unwrap();
+        v.push(s)
+    }
+    (r, c, v)
 }
 
 #[inline]
@@ -29,4 +94,214 @@ where
         .split_whitespace()
         .map(|c| T::from_str(c).unwrap())
         .collect()
+}
+
+pub mod dinic_library {
+    /// edges is expressed as adjacency list.
+    #[derive(Clone, Debug)]
+    pub struct Dinic<T> {
+        edges: Vec<Vec<Edge<T>>>,
+        edges_size: usize,
+    }
+
+    #[derive(Copy, Clone, Debug)]
+    struct Edge<T> {
+        to: usize,
+        capacity: T,
+        rev: usize,
+    }
+
+    impl<T> Dinic<T>
+    where
+        T: std::convert::From<i32> + Clone + Copy,
+    {
+        #[inline]
+        fn add_edge(&mut self, from: usize, to: usize, capacity: T) {
+            unsafe {
+                let rev = self.edges.get_unchecked(to).len();
+                self.edges
+                    .get_unchecked_mut(from)
+                    .push(Edge { to, capacity, rev });
+                let rev = self.edges.get_unchecked(from).len() - 1;
+                self.edges.get_unchecked_mut(to).push(Edge {
+                    to: from,
+                    capacity: T::from(0i32),
+                    rev,
+                });
+            }
+        }
+
+        #[inline]
+        pub fn new(n: usize, edges: &[(usize, usize, T)]) -> Self {
+            let mut container = Self {
+                edges: vec![vec![]; n],
+                edges_size: edges.len(),
+            };
+            for &(from, to, capacity) in edges {
+                container.add_edge(from, to, capacity);
+            }
+            container
+        }
+    }
+
+    impl<T> Dinic<T>
+    where
+        T: std::convert::From<i32>
+            + std::cmp::Ord
+            + Copy
+            + std::ops::SubAssign
+            + std::ops::AddAssign
+            + Max,
+    {
+        #[inline]
+        fn bfs(&self, start: usize) -> Vec<i64> {
+            unsafe {
+                let mut level = vec![-1; self.edges.len()];
+                let mut queue = std::collections::VecDeque::with_capacity(self.edges_size);
+                *level.get_unchecked_mut(start) = 0;
+                queue.push_back(start);
+                while !queue.is_empty() {
+                    let vertex = queue.pop_front().unwrap();
+                    (0..self.edges.get_unchecked(vertex).len()).fold((), |_, i| {
+                        let edge = self.edges.get_unchecked(vertex).get_unchecked(i);
+                        if edge.capacity > T::from(0) && *level.get_unchecked(edge.to) < 0 {
+                            *level.get_unchecked_mut(edge.to) = *level.get_unchecked(vertex) + 1;
+                            queue.push_back(edge.to);
+                        }
+                    });
+                }
+                level
+            }
+        }
+
+        #[inline]
+        fn dfs(
+            &mut self,
+            vertex: usize,
+            terminal: usize,
+            flow: T,
+            level: &[i64],
+            iter: &mut [usize],
+        ) -> T {
+            if vertex == terminal {
+                flow
+            } else {
+                unsafe {
+                    let mut d = T::from(0);
+                    for i in *iter.get_unchecked(vertex)..self.edges.get_unchecked(vertex).len() {
+                        let edge = *self.edges.get_unchecked(vertex).get_unchecked(i);
+                        if edge.capacity > T::from(0)
+                            && *level.get_unchecked(vertex) < *level.get_unchecked(edge.to)
+                        {
+                            d = self.dfs(
+                                edge.to,
+                                terminal,
+                                std::cmp::min(flow, edge.capacity),
+                                level,
+                                iter,
+                            );
+                            if d > T::from(0) {
+                                {
+                                    self.edges
+                                        .get_unchecked_mut(vertex)
+                                        .get_unchecked_mut(i)
+                                        .capacity -= d;
+                                }
+                                self.edges
+                                    .get_unchecked_mut(edge.to)
+                                    .get_unchecked_mut(edge.rev)
+                                    .capacity += d;
+                                break;
+                            }
+                        }
+                        d = T::from(0);
+                    }
+                    d
+                }
+            }
+        }
+
+        #[inline]
+        pub fn max_flow(&mut self, start: usize, terminal: usize) -> T {
+            let mut flow = T::from(0);
+            let mut iter = vec![0usize; self.edges.len()];
+            loop {
+                unsafe {
+                    let level = self.bfs(start);
+                    if *level.get_unchecked(terminal) < 0 {
+                        return flow;
+                    }
+                    let mut f;
+                    while {
+                        f = self.dfs(start, terminal, T::MAX, &level, &mut iter);
+                        f > T::from(0) && f != T::MAX
+                    } {
+                        flow += f;
+                    }
+                    iter = iter.iter().map(|_| 0).collect();
+                }
+            }
+        }
+    }
+
+    /// Return T::MAX
+    pub trait Max
+    where
+        Self: Copy,
+    {
+        const MAX: Self;
+    }
+
+    impl Max for i32 {
+        const MAX: Self = std::i32::MAX;
+    }
+
+    impl Max for u32 {
+        const MAX: Self = std::u32::MAX;
+    }
+
+    impl Max for i64 {
+        const MAX: Self = std::i64::MAX;
+    }
+
+    impl Max for u64 {
+        const MAX: Self = std::u64::MAX;
+    }
+
+    impl Max for i128 {
+        const MAX: Self = std::i128::MAX;
+    }
+
+    impl Max for u128 {
+        const MAX: Self = std::u128::MAX;
+    }
+
+    impl Max for usize {
+        const MAX: Self = std::usize::MAX;
+    }
+
+    impl Max for isize {
+        const MAX: Self = std::isize::MAX;
+    }
+
+    #[cfg(test)]
+    mod tests_dinic {
+        use super::*;
+
+        #[test]
+        fn for_maximum_flow_dinic1() {
+            let n = 4;
+            let vertics = [(0, 1, 2i64), (0, 2, 1), (1, 2, 1), (1, 3, 1), (2, 3, 2)];
+            let mut flow = Dinic::new(n, &vertics);
+            assert_eq!(flow.max_flow(0, 3), 3);
+        }
+
+        #[test]
+        fn for_maximum_flow_dinic2() {
+            let n = 2;
+            let vertics = [(0, 1, 1000i64)];
+            let mut flow = Dinic::new(n, &vertics);
+            assert_eq!(flow.max_flow(0, 1), 1000);
+        }
+    }
 }
