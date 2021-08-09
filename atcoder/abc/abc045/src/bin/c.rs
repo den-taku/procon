5908331d5b6c4feff9106bb351d86f852666c721
dentaku <dentaku1412@gmail.com>
2021-08-09 15:53:06 +0900

    AC ABC045 C

--- atcoder/abc/abc045/src/bin/c.rs
+++ atcoder/abc/abc045/src/bin/c.rs
@@ -4,8 +4,27 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        s: String
     }
-    unimplemented!()
+    let mut v = Vec::new();
+    for c in s.chars() {
+        v.push(c as u64 - b'0' as u64);
+    }
+    v.reverse();
+    let mut ans = 0;
+    for i in 0..1 << (v.len() - 1) {
+        let mut digit = 1;
+        let mut sum = 0;
+        for j in 0..v.len() {
+            if (i >> j) & 1 == 0 {
+                sum += v[j] * digit;
+                digit *= 10;
+            } else {
+                sum += v[j] * digit;
+                digit = 1;
+            }
+        }
+        ans += sum;
+    }
+    println!("{}", ans);
 }
