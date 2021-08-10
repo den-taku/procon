ad390a76c38e5f74ef2779da9399f1e0bf37309f
dentaku <dentaku1412@gmail.com>
2021-08-10 17:43:49 +0900

    AC ABC015 D

--- atcoder/abc/abc015/src/bin/d.rs
+++ atcoder/abc/abc015/src/bin/d.rs
@@ -1,11 +1,31 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::cmp::max;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        w: usize,
+        n: usize,
+        k: usize,
+        ab: [(usize, usize); n]
     }
-    unimplemented!()
+    let mut dp = vec![0usize; (n + 1) * (k + 1) * (w + 1)];
+    for a in 1..w + 1 {
+        for b in 1..k + 1 {
+            for c in 1..n + 1 {
+                if a >= ab[c - 1].0 {
+                    dp[a * (k + 1) * (n + 1) + b * (n + 1) + c] = max(
+                        dp[a * (k + 1) * (n + 1) + b * (n + 1) + c - 1],
+                        dp[(a - ab[c - 1].0) * (k + 1) * (n + 1) + (b - 1) * (n + 1) + c - 1]
+                            + ab[c - 1].1,
+                    )
+                } else {
+                    dp[a * (k + 1) * (n + 1) + b * (n + 1) + c] =
+                        dp[a * (k + 1) * (n + 1) + b * (n + 1) + c - 1];
+                }
+            }
+        }
+    }
+    println!("{}", dp[w * (k + 1) * (n + 1) + k * (n + 1) + n]);
 }
