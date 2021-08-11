c0c6f73817bbe0d5544a8d91f3ecef6ef132be8e
dentaku <dentaku1412@gmail.com>
2021-08-11 13:33:13 +0900

    AC ARC037 B

--- atcoder/arc/arc037/src/bin/b.rs
+++ atcoder/arc/arc037/src/bin/b.rs
@@ -1,11 +1,137 @@
+#![allow(dead_code)]
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        edges: [(usize, usize); m]
     }
-    unimplemented!()
+    let mut valid = vec![true; n];
+    let mut uft = union_find_library::UnionFind::new(n);
+    for &(a, b) in &edges {
+        if !uft.unite(a - 1, b - 1) {
+            valid[a - 1] = false;
+        }
+    }
+    for (i, b) in valid.clone().into_iter().enumerate() {
+        if !b {
+            let representative = uft.find(i);
+            valid[representative] = false;
+        }
+    }
+    let mut count = std::collections::HashSet::with_capacity(n);
+    let mut representative = 0;
+    for (i, b) in valid.iter().copied().enumerate() {
+        if b && valid[{
+            representative = uft.find(i);
+            representative
+        }] {
+            count.insert(representative);
+        }
+    }
+    println!("{}", count.len());
+}
+
+pub mod union_find_library {
+    /// Union-Find Tree, that treats disjoint sets efficiently.
+    /// verified by this(https://atcoder.jp/contests/atc001/submissions/24929276).
+    pub struct UnionFind {
+        par: Vec<usize>,
+        rank: Vec<usize>,
+    }
+
+    impl UnionFind {
+        #[inline]
+        /// Create a new Union-Find Tree contains n elements.
+        /// At the first state, the elements are mutually disjoint.
+        pub fn new(n: usize) -> Self {
+            UnionFind {
+                par: (0..n).collect(),
+                rank: vec![0; n],
+            }
+        }
+
+        #[inline]
+        /// Return representative representing set containing x.
+        pub fn find(&mut self, x: usize) -> usize {
+            if x >= self.par.len() {
+                panic!("out of bound.")
+            }
+            unsafe {
+                if *self.par.get_unchecked(x) == x {
+                    x
+                } else {
+                    let mut represent = x;
+                    while {
+                        represent = *self.par.get_unchecked(represent);
+                        *self.par.get_unchecked(represent) != represent
+                    } {}
+                    *self.par.get_unchecked_mut(x) = represent;
+                    represent
+                }
+            }
+        }
+
+        #[inline]
+        /// Unite 2 sets, one containing x and the other containing y.
+        pub fn unite(&mut self, x: usize, y: usize) -> bool {
+            let x_par = self.find(x);
+            let y_par = self.find(y);
+            if x_par != y_par {
+                unsafe {
+                    if *self.rank.get_unchecked(x_par) < *self.rank.get_unchecked(y_par) {
+                        *self.par.get_unchecked_mut(x_par) = y_par;
+                    } else {
+                        *self.par.get_unchecked_mut(y_par) = x_par;
+                        if *self.rank.get_unchecked(x_par) == *self.rank.get_unchecked(y_par) {
+                            self.rank[x_par] += 1;
+                        }
+                    }
+                }
+            }
+            x_par != y_par
+        }
+
+        #[inline]
+        /// Decide whether set, containing x, contains y or not.
+        pub fn same(&mut self, x: usize, y: usize) -> bool {
+            self.find(x) == self.find(y)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests_union_find {
+        use super::*;
+
+        #[test]
+        fn for_union_find() {
+            let queries = [
+                (0, 1, 2),
+                (0, 3, 2),
+                (1, 1, 3),
+                (1, 1, 4),
+                (0, 2, 4),
+                (1, 4, 1),
+                (0, 4, 2),
+                (0, 0, 0),
+                (1, 0, 0),
+            ];
+            let ans = [true, false, true, true];
+            let n = 8;
+            let mut uf_tree = UnionFind::new(n);
+            let mut index = 0;
+            for (i, x, y) in queries {
+                match i {
+                    0 => uf_tree.unite(x, y),
+                    _ => {
+                        assert_eq!(uf_tree.same(x, y), ans[index]);
+                        index += 1;
+                    }
+                }
+            }
+        }
+    }
 }
