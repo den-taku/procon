15bde937de6fb2141c91937832642de404b126ac
dentaku <dentaku1412@gmail.com>
2021-08-12 01:00:44 +0900

    AC ABC002 D

--- atcoder/abc/abc002/src/bin/d.rs
+++ atcoder/abc/abc002/src/bin/d.rs
@@ -1,11 +1,38 @@
 #![allow(unreachable_code)]
-use proconio::{fastout, input};
+use proconio::input;
 
-#[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        relations: [(usize, usize); m]
     }
-    unimplemented!()
+    let relations = relations.iter().collect::<std::collections::HashSet<_>>();
+    let mut ans = 0;
+    'out: for i in 0..1 << n {
+        for j in 0..n {
+            for k in j + 1..n {
+                if i >> j & 1 == 1
+                    && i >> k & 1 == 1
+                    && relations.get(&(j + 1, k + 1)).is_none()
+                    && relations.get(&(k + 1, j + 1)).is_none()
+                {
+                    continue 'out;
+                }
+            }
+        }
+        let mut count = 0;
+        let mut i = i;
+        if i & 1 == 1 {
+            count += 1;
+        }
+        for _ in 0..n - 1 {
+            i >>= 1;
+            if i & 1 == 1 {
+                count += 1;
+            }
+        }
+        ans = std::cmp::max(ans, count);
+    }
+    println!("{}", ans);
 }
