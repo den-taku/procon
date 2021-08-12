6e3a0329a21e56c33f1c8dba16db3d5ac4112da9
dentaku <dentaku1412@gmail.com>
2021-08-12 15:16:13 +0900

    AC Typical90 001

--- others/typical90/src/bin/001.rs
+++ others/typical90/src/bin/001.rs
@@ -4,8 +4,46 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        l: usize,
+        k: usize,
+        cuts: [usize; n]
     }
-    unimplemented!()
+    println!("{}", binary_search(k, l, &cuts));
+}
+
+#[inline]
+fn binary_search(k: usize, l: usize, cuts: &[usize]) -> usize {
+    let mut upper_bound = l / k;
+    let mut lower_bound = 0;
+    while upper_bound - lower_bound > 1 {
+        let est = (upper_bound + lower_bound) / 2;
+        if condition(k, l, est, cuts) {
+            lower_bound = est;
+        } else {
+            upper_bound = est;
+        }
+    }
+    if condition(k, l, upper_bound, cuts) {
+        upper_bound
+    } else {
+        lower_bound
+    }
+}
+
+#[inline]
+fn condition(k: usize, l: usize, est: usize, cuts: &[usize]) -> bool {
+    let mut last = 0;
+    let mut index = 0;
+    'out: for _ in 0..k {
+        for i in index..cuts.len() {
+            if cuts[i] - last >= est {
+                last = cuts[i];
+                index = i;
+                continue 'out;
+            }
+        }
+        return false;
+    }
+    l - cuts[index] >= est
 }
