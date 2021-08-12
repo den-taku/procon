e61029d4f4ee4948fbbd66a0d9d1b1aacba4af29
dentaku <dentaku1412@gmail.com>
2021-08-13 03:06:00 +0900

    AC ABC054 C

--- atcoder/abc/abc054/src/bin/c.rs
+++ atcoder/abc/abc054/src/bin/c.rs
@@ -1,11 +1,26 @@
 #![allow(unreachable_code)]
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        edges: [(usize, usize); m]
     }
-    unimplemented!()
+    let edges = edges
+        .into_iter()
+        .flat_map(|(x, y)| vec![(x, y), (y, x)].into_iter())
+        .collect::<std::collections::HashSet<_>>();
+    println!(
+        "{}",
+        (2..n + 1)
+            .permutations(n - 1)
+            .filter(|permutation| edges.contains(&(1, permutation[0]))
+                && permutation
+                    .windows(2)
+                    .all(|v| edges.contains(&(v[0], v[1]))))
+            .count()
+    )
 }
