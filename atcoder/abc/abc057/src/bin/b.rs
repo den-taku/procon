587784b5a93e0f8fc6807d3f485c19ca977b1833
dentaku <dentaku1412@gmail.com>
2021-08-14 13:21:50 +0900

    AC ABC057 B

--- atcoder/abc/abc057/src/bin/b.rs
+++ atcoder/abc/abc057/src/bin/b.rs
@@ -4,8 +4,21 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        students: [(i64, i64); n],
+        points: [(i64, i64); m]
     }
-    unimplemented!()
+    for &(x, y) in &students {
+        let mut index = 0;
+        let mut distance = std::i64::MAX;
+        for (i, &(a, b)) in points.iter().enumerate() {
+            let dis = (x - a).abs() + (y - b).abs();
+            if dis < distance {
+                distance = dis;
+                index = i + 1;
+            }
+        }
+        println!("{}", index);
+    }
 }
