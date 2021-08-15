fad8b1257eabef710a8a01514420a2137ffa446a
dentaku <dentaku1412@gmail.com>
2021-08-15 14:37:41 +0900

    AC Typical90 003

--- others/typical90/src/bin/003.rs
+++ others/typical90/src/bin/003.rs
@@ -4,8 +4,48 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        edges: [(usize, usize); n - 1]
     }
-    unimplemented!()
+    let mut adj = vec![Vec::new(); n];
+    for &(x, y) in &edges {
+        adj[x - 1].push(y - 1);
+        adj[y - 1].push(x - 1);
+    }
+    let mut dis = vec![None; n];
+    dis[0] = Some(0);
+    let mut queue = std::collections::VecDeque::new();
+    queue.push_back(0);
+    while let Some(v) = queue.pop_front() {
+        let d = dis[v].unwrap() + 1;
+        for &e in &adj[v] {
+            if dis[e].is_none() {
+                dis[e] = Some(d);
+                queue.push_back(e);
+            }
+        }
+    }
+    let max_index = dis
+        .iter()
+        .enumerate()
+        .max_by_key(|(_i, &e)| e.unwrap())
+        .unwrap()
+        .0;
+    let mut dis = vec![None; n];
+    dis[max_index] = Some(1);
+    let mut queue = std::collections::VecDeque::new();
+    queue.push_back(max_index);
+    while let Some(v) = queue.pop_front() {
+        let d = dis[v].unwrap() + 1;
+        for &e in &adj[v] {
+            if dis[e].is_none() {
+                dis[e] = Some(d);
+                queue.push_back(e);
+            }
+        }
+    }
+    println!(
+        "{}",
+        dis.iter().max_by_key(|e| e.unwrap()).unwrap().unwrap()
+    )
 }
