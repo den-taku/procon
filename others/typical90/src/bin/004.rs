f29ee910b1ec349d0875b849b256649ea4e751fb
dentaku <dentaku1412@gmail.com>
2021-08-16 11:20:15 +0900

    AC Typical90 004

--- others/typical90/src/bin/004.rs
+++ others/typical90/src/bin/004.rs
@@ -4,8 +4,28 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        h: usize,
+        w: usize,
+        a: [u32; h * w]
     }
-    unimplemented!()
+    let sumh = {
+        let mut v = vec![0; h];
+        for (i, p) in v.iter_mut().enumerate() {
+            *p = a.iter().skip(i * w).take(w).sum::<u32>();
+        }
+        v
+    };
+    let sumw = {
+        let mut v = vec![0; w];
+        for (j, p) in v.iter_mut().enumerate() {
+            *p = a.iter().skip(j).step_by(w).sum::<u32>();
+        }
+        v
+    };
+    for (i, &si) in sumh.iter().enumerate() {
+        for (j, &sj) in sumw.iter().enumerate() {
+            print!("{} ", si + sj - a[i * w + j])
+        }
+        println!("");
+    }
 }
