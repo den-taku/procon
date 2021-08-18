1cc793eef9023964f57d18061a4115a34eaef271
dentaku <dentaku1412@gmail.com>
2021-08-18 14:42:47 +0900

    AC JOI2009A

--- others/joi2008yo/src/bin/a.rs
+++ others/joi2008yo/src/bin/a.rs
@@ -4,8 +4,14 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: i32,
     }
-    unimplemented!()
+    let mut n = 1000 - n;
+    let mut count = 0;
+    let coins = [500, 100, 50, 10, 5, 1];
+    for coin in coins.iter() {
+        count += n / coin;
+        n %= coin;
+    }
+    println!("{}", count);
 }
