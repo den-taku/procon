663dd800c6ded85bf00fc81f2667676f96a91755
dentaku <dentaku1412@gmail.com>
2021-08-20 20:44:03 +0900

    AC ARC068 C

--- atcoder/arc/arc068/src/bin/c.rs
+++ atcoder/arc/arc068/src/bin/c.rs
@@ -4,8 +4,15 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        x: u64
     }
-    unimplemented!()
+    let div = x / 11;
+    let mo = if x % 11 > 6 {
+        2
+    } else if x % 11 == 0 {
+        0
+    } else {
+        1
+    };
+    println!("{}", mo + div * 2);
 }
--- others/tdpc/src/bin/a.rs
+++ others/tdpc/src/bin/a.rs
@@ -4,8 +4,26 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        points: [usize; n]
     }
-    unimplemented!()
+    let max: usize = points.iter().sum();
+    let mut dp = vec![true; (n + 1) * (max + 1)];
+    let mut count = 1;
+    for i in 1..max + 1 {
+        let mut valid = false;
+        dp[i * (n + 1)] = false;
+        for (j, &p) in points.iter().enumerate() {
+            if valid || (i >= p && dp[(i - p) * (n + 1) + j]) {
+                valid = true;
+                break;
+            } else {
+                dp[i * (n + 1) + j + 1] = false;
+            }
+        }
+        if valid {
+            count += 1;
+        }
+    }
+    println!("{}", count)
 }
