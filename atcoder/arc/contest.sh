83ee67e9ff8d518491d9e6e52980d41b9384e440
dentaku <dentaku1412@gmail.com>
2021-08-23 01:29:40 +0900

    Cannot open, so echo

--- atcoder/arc/contest.sh
+++ atcoder/arc/contest.sh
@@ -7,8 +7,11 @@ cargo compete new $CONTEST_NAME
 
 cd $CONTEST_NAME
 
-cargo compete open
-
 for f in ./src/bin/*; do
     open $f
 done
+
+# cargo compete open
+for f in ./src/bin/*; do
+    echo "open https://atcoder.jp/contests/$CONTEST_NAME/tasks/$CONTEST_NAME"_"`basename ${f} .rs`";
+done | tail -r
