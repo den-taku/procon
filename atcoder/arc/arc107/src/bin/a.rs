a20f4135e07219e1bca192875334e564d3579a76
dentaku <dentaku1412@gmail.com>
2021-08-23 08:26:21 +0900

    AC ARC107 A

--- atcoder/arc/arc107/src/bin/a.rs
+++ atcoder/arc/arc107/src/bin/a.rs
@@ -1,11 +1,13 @@
-#![allow(unreachable_code)]
-use proconio::{fastout, input};
-
-#[fastout]
+#[proconio::fastout]
 fn main() {
-    input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+    proconio::input! {
+        a: u64,
+        b: u64,
+        c: u64
     }
-    unimplemented!()
+    let m = 998244353;
+    let a_sum = (a * (a + 1) >> 1) % m;
+    let b_sum = (b * (b + 1) >> 1) % m;
+    let c_sum = (c * (c + 1) >> 1) % m;
+    println!("{}", a_sum * b_sum % m * c_sum % m)
 }
