75c55ad137bb8664dae85846e0ca70416ca30123
dentaku <dentaku1412@gmail.com>
2021-08-26 13:21:51 +0900

    AC ABC153 E

--- atcoder/abc/abc153/src/bin/e.rs
+++ atcoder/abc/abc153/src/bin/e.rs
@@ -4,8 +4,53 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        h: usize,
+        n: usize,
+        magics: [(usize, usize); n]
     }
-    unimplemented!()
+    let mut dp = Vec::new();
+    dp.push(Some(0));
+    for i in 1..h {
+        let mut est = None;
+        for &(damage, cost) in &magics {
+            if i >= damage {
+                if let Some(s) = dp[dp.len() - damage] {
+                    if let Some(e) = est {
+                        if s + cost < e {
+                            est = Some(s + cost)
+                        }
+                    } else {
+                        est = Some(s + cost)
+                    }
+                }
+            }
+        }
+        dp.push(est);
+    }
+    for i in h..h + magics.iter().max_by_key(|e| e.0).unwrap().0 + 1 {
+        let mut est = None;
+        for &(damage, cost) in &magics {
+            if i >= damage {
+                if let Some(s) = dp[dp.len() - damage] {
+                    if let Some(e) = est {
+                        if s + cost < e {
+                            est = Some(s + cost)
+                        }
+                    } else {
+                        est = Some(s + cost)
+                    }
+                }
+            }
+        }
+        dp.push(est);
+    }
+    println!(
+        "{}",
+        dp.iter()
+            .skip(h)
+            .filter(|e| if let Some(_) = e { true } else { false })
+            .map(|e| e.unwrap())
+            .min()
+            .unwrap()
+    );
 }
