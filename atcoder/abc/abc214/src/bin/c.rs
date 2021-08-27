996756f2c0db07901534151d030862194b79fe7b
dentaku <dentaku1412@gmail.com>
2021-08-27 16:26:40 +0900

    AC ABC214 C

--- atcoder/abc/abc214/src/bin/c.rs
+++ atcoder/abc/abc214/src/bin/c.rs
@@ -4,8 +4,33 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        s: [u64; n],
+        mut t: [u64; n]
     }
-    unimplemented!()
+    let mut visited = vec![false; n];
+    for (i, mut time) in t.clone().iter().copied().enumerate() {
+        if visited[i] && time >= t[i] {
+            continue;
+        }
+        visited[i] = true;
+        time += s[i];
+        for j in 1..n {
+            let index = (i + j) % n;
+            if time < t[index] || (time == t[index] && !visited[index]) {
+                visited[index] = true;
+                t[index] = time;
+                time += s[index];
+            } else {
+                break;
+            }
+        }
+    }
+    println!(
+        "{}",
+        t.iter()
+            .map(|&n| n.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
