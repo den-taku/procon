d42e48c165f45d384cf1658e845b86a0766df711
dentaku <dentaku1412@gmail.com>
2021-08-28 14:42:58 +0900

    Need to implement

--- atcoder/abc/abc214/src/bin/d.rs
+++ atcoder/abc/abc214/src/bin/d.rs
@@ -1,11 +1,25 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        mut edges: [(usize, usize, u64); n - 1]
     }
+    edges.sort_by_key(|e| e.2);
+    let mut adjacent = vec![HashSet::new(); n];
+    for &(a, b, _) in &edges {
+        adjacent[a - 1].insert(b - 1);
+        adjacent[b - 1].insert(a - 1);
+    }
+    let mut ans = 0;
+    for &(a, b, w) in edges.iter().rev() {
+        ans += w * adjacent[a - 1].len() as u64 * adjacent[b - 1].len() as u64;
+        adjacent[a - 1].remove(&(b - 1));
+        adjacent[b - 1].remove(&(a - 1));
+    }
+    println!("{}", ans);
     unimplemented!()
 }
