cdce3e93654cd88c92bca4a1d9910565ce5f0906
dentaku <dentaku1412@gmail.com>
2021-08-29 23:53:01 +0900

    AC ABC216 D

--- atcoder/abc/abc216/src/bin/d.rs
+++ atcoder/abc/abc216/src/bin/d.rs
@@ -1,10 +1,59 @@
 #![allow(unreachable_code)]
 use proconio::fastout;
-use std::boxed::Box;
 
 #[fastout]
 fn main() {
-    let (_n, _ks, _a_s) = input();
+    let (n, ks, a_s) = input();
+    let mut indexes = vec![0; ks.len()];
+    let mut count = vec![None; n];
+    let mut top = 0u64;
+    let mut tmp = 0i128;
+    let mut cand = std::collections::VecDeque::new();
+    for (i, e) in a_s
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| ks[*i] > 0)
+        .map(|(i, v)| (i, v[0]))
+    {
+        top += 1;
+        if let Some(idx) = count[(e - 1) as usize] {
+            indexes[idx] += 1;
+            indexes[i] += 1;
+            cand.push_back(idx);
+            cand.push_back(i);
+            count[(e - 1) as usize] = None;
+            tmp -= 2;
+        } else {
+            count[(e - 1) as usize] = Some(i);
+        }
+    }
+    let mut culm = (top as i128 + tmp) as u64;
+    while let Some(cnd) = cand.pop_front() {
+        if culm == top {
+            break;
+        } else {
+            culm += 1;
+            if indexes[cnd] == ks[cnd] {
+                continue;
+            }
+            let e = a_s[cnd][indexes[cnd]];
+            if let Some(idx) = count[(e - 1) as usize] {
+                indexes[idx] += 1;
+                indexes[cnd] += 1;
+                cand.push_back(idx);
+                cand.push_back(cnd);
+                count[(e - 1) as usize] = None;
+                culm -= 2;
+            } else {
+                count[(e - 1) as usize] = Some(cnd);
+            }
+        }
+    }
+    if indexes.iter().zip(ks.iter()).all(|e| e.0 == e.1) {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
 
 #[inline(always)]
