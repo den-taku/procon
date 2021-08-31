0ef8f80568fe8a57689d986718f23b244cfa3269
dentaku <dentaku1412@gmail.com>
2021-09-01 03:31:50 +0900

    AC ARC113 B

--- atcoder/arc/arc113/src/bin/b.rs
+++ atcoder/arc/arc113/src/bin/b.rs
@@ -4,8 +4,35 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        a: usize,
+        b: usize,
+        c: usize
     }
-    unimplemented!()
+    let t = [1, 1, 4, 4, 2, 1, 1, 4, 4, 2];
+    println!("{}", pow(a % 10, index(b, c, t[a % 10]), 10))
+}
+
+#[inline(always)]
+fn pow(a: usize, x: usize, m: usize) -> usize {
+    if x == 0 {
+        1
+    } else if x == 1 {
+        a % m
+    } else if x % 2 == 0 {
+        let a_x_2 = pow(a, x / 2, m);
+        (a_x_2 * a_x_2) % m
+    } else {
+        let a_x_2 = pow(a, x / 2, m);
+        (a_x_2 * a_x_2 * a) % m
+    }
+}
+
+#[inline(always)]
+fn index(b: usize, c: usize, t: usize) -> usize {
+    let pow_num = pow(b, c, t);
+    if pow_num == 0 {
+        t
+    } else {
+        pow_num
+    }
 }
