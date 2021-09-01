5e3a804e41a6c7f1122ad99593f790482a399e76
dentaku <dentaku1412@gmail.com>
2021-09-01 16:00:16 +0900

    AC ABC091 B

--- atcoder/abc/abc091/src/bin/b.rs
+++ atcoder/abc/abc091/src/bin/b.rs
@@ -1,10 +1,38 @@
 use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
     input! {
         n: usize,
-        t: [[i128; 50];50]
+        s: [String; n],
+        m: usize,
+        t: [String; m]
     }
-    unimplemented!()
+    let mut blue = HashMap::new();
+    for b in s.clone() {
+        if let Some(&v) = blue.get(&b) {
+            blue.insert(b, v + 1);
+        } else {
+            blue.insert(b, 1);
+        }
+    }
+    let mut red = HashMap::new();
+    for r in t {
+        if let Some(&v) = red.get(&r) {
+            red.insert(r, v + 1);
+        } else {
+            red.insert(r, 1);
+        }
+    }
+    println!(
+        "{}",
+        std::cmp::max(
+            s.into_iter()
+                .map(|b| { blue.get(&b).unwrap() - red.get(&b).unwrap_or(&0) })
+                .max()
+                .unwrap(),
+            0
+        )
+    )
 }
