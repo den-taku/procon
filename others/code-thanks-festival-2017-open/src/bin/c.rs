d71c855080b08566bbe7422dc30e3be88346ade2
dentaku <dentaku1412@gmail.com>
2021-09-01 15:36:09 +0900

    AC CTF2017 C

--- others/code-thanks-festival-2017-open/src/bin/c.rs
+++ others/code-thanks-festival-2017-open/src/bin/c.rs
@@ -1,11 +1,27 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        k: usize,
+        ab: [(usize, usize); n]
     }
-    unimplemented!()
+    let mut heap = ab
+        .into_iter()
+        .map(|e| Reverse(e))
+        .collect::<BinaryHeap<_>>();
+    println!(
+        "{}",
+        (0..k)
+            .map(|_| {
+                let Reverse(e) = heap.pop().unwrap();
+                heap.push(Reverse((e.0 + e.1, e.1)));
+                e.0
+            })
+            .sum::<usize>()
+    )
 }
