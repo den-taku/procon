033a3e6b29d8b22af6e5386c025d0f40aed8a11f
dentaku <dentaku1412@gmail.com>
2021-09-03 20:30:12 +0900

    AC ABC160 D

--- atcoder/abc/abc160/src/bin/d.rs
+++ atcoder/abc/abc160/src/bin/d.rs
@@ -4,8 +4,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: isize,
+        x: isize,
+        y: isize
     }
-    unimplemented!()
+    let mut ans = vec![0; n as usize - 1];
+    for i in 1..n {
+        for j in i + 1..n + 1 {
+            let strait = j - i;
+            let short = (x - i).abs() + 1 + (j - y).abs();
+            ans[std::cmp::min(strait, short) as usize - 1] += 1;
+        }
+    }
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(|e| e.to_string())
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
 }
