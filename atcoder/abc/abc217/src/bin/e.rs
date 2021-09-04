263f9fa2ffb95d7fd8357cb9db049851fc17a28a
dentaku <dentaku1412@gmail.com>
2021-09-04 22:56:31 +0900

    AC ABC217 E

--- atcoder/abc/abc217/src/bin/e.rs
+++ atcoder/abc/abc217/src/bin/e.rs
@@ -1,76 +1,32 @@
 #![allow(unreachable_code)]
 use proconio::fastout;
-use std::collections::{BTreeSet, HashMap, VecDeque};
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, VecDeque};
 use Query::*;
 
 #[fastout]
 fn main() {
     let queries = input();
-    let mut tree = BTreeSet::new();
-    let mut next = BTreeSet::new();
+    let mut heap = BinaryHeap::new();
     let mut array = VecDeque::new();
-    let mut count = HashMap::<usize, usize>::new();
-    let mut count_next = HashMap::<usize, usize>::new();
-    let mut count_buffer = HashMap::<usize, usize>::new();
-    for (i, query) in queries.into_iter().enumerate() {
+    for query in queries {
         match query {
             One(x) => {
                 array.push_back(x);
-                if !next.insert(x) {
-                    if let Some(&v) = count_next.get(&x) {
-                        count_next.insert(x, v + 1);
-                        count_buffer.insert(x, v + 1);
-                    } else {
-                        count_next.insert(x, 1);
-                        count_buffer.insert(x, 1);
-                    }
-                }
             }
             Two => {
-                if tree.len() == 0 {
+                if heap.len() == 0 {
                     let a = array.pop_front().unwrap();
                     println!("{}", a);
-                    if !(next.len() == 0) {
-                        if let Some(&v) = count_next.get(&a) {
-                            if v == 1 {
-                                count_next.remove(&a);
-                                count_buffer.remove(&a);
-                            } else {
-                                count_next.insert(a, v - 1);
-                                count_buffer.insert(a, v - 1);
-                            }
-                        } else {
-                            next.remove(&a);
-                        }
-                    }
                 } else {
-                    let &e = tree.range(0..).next().unwrap();
+                    let Reverse(e) = heap.pop().unwrap();
                     println!("{}", e);
-                    if let Some(&v) = count.get(&e) {
-                        if v == 1 {
-                            count.remove(&e);
-                        } else {
-                            count.insert(e, v - 1);
-                        }
-                        let &v = count_next.get(&e).unwrap();
-                        if v == 1 {
-                            count_next.remove(&e);
-                            count_buffer.remove(&e);
-                        } else {
-                            count_next.insert(e, v - 1);
-                            count_buffer.insert(e, v - 1);
-                        }
-                        tree.insert(e);
-                    } else {
-                        next.remove(&e);
-                        tree.remove(&e);
-                    }
                 }
             }
             Three => {
-                tree = &tree | &next;
-                count = count_next;
-                count_next = HashMap::new();
+                for e in array {
+                    heap.push(Reverse(e));
+                }
                 array = VecDeque::new();
             }
         }
