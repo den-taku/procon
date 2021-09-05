f0eb5146eb1233f64f804fc3a08876cd72b8dc8a
dentaku <dentaku1412@gmail.com>
2021-09-05 15:34:45 +0900

    AC ABC216 D

--- atcoder/abc/abc126/src/bin/d.rs
+++ atcoder/abc/abc126/src/bin/d.rs
@@ -4,8 +4,36 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        edges: [(usize, usize, usize); n - 1]
     }
-    unimplemented!()
+    let mut adjacent = vec![Vec::new(); n];
+    for (u, v, w) in edges {
+        adjacent[u - 1].push((v - 1, w));
+        adjacent[v - 1].push((u - 1, w));
+    }
+    let mut color = vec![None; n];
+    color[0] = Some(0);
+    let mut queue = std::collections::VecDeque::new();
+    queue.push_back(0);
+    while let Some(i) = queue.pop_front() {
+        for &(v, w) in &adjacent[i] {
+            if color[v].is_none() {
+                if w % 2 == 0 {
+                    color[v] = Some(color[i].unwrap());
+                } else {
+                    color[v] = Some(color[i].unwrap() ^ 1);
+                }
+                queue.push_back(v);
+            }
+        }
+    }
+    println!(
+        "{}",
+        color
+            .iter()
+            .map(|v| format!("{}\n", v.unwrap()))
+            .collect::<String>()
+            .trim()
+    );
 }
