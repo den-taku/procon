6cb2c4cf889d34c5803a3ce84037c415e030e9e4
dentaku <dentaku1412@gmail.com>
2021-09-11 19:08:17 +0900

    AC ABC113 C

--- atcoder/abc/abc113/src/bin/c.rs
+++ atcoder/abc/abc113/src/bin/c.rs
@@ -5,7 +5,23 @@ use proconio::{fastout, input};
 fn main() {
     input! {
         _n: usize,
-        _t: [[i128; 50];50]
+        m: usize,
+        py: [(u64, u64); m]
     }
-    unimplemented!()
+    let mut map = std::collections::HashMap::new();
+    for (p, y) in &py {
+        map.entry(p).or_insert_with(Vec::new).push(*y);
+    }
+    for (_, v) in map.iter_mut() {
+        v.sort();
+    }
+    let mut ans = Vec::new();
+    for (p, y) in &py {
+        ans.push(format!(
+            "{:06}{:06}",
+            p,
+            map[p].binary_search(&y).unwrap() + 1
+        ));
+    }
+    println!("{}", ans.join("\n"));
 }
