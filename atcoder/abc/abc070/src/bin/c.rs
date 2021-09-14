c0e8d7b7c2bd6e629c5c1d538a42f755a774b9eb
dentaku <dentaku1412@gmail.com>
2021-09-14 15:48:22 +0900

    ReAC ABC070 C

--- atcoder/abc/abc070/src/bin/c.rs
+++ atcoder/abc/abc070/src/bin/c.rs
@@ -1,4 +1,5 @@
 #![allow(unreachable_code)]
+#[allow(dead_code)]
 use proconio::{fastout, input};
 
 #[fastout]
@@ -11,25 +12,69 @@ fn main() {
     t.dedup();
     let mut ans = 1;
     for e in t {
-        ans = lcm(ans, e);
+        ans = number_library::lcm(ans, e);
     }
     println!("{}", ans);
 }
 
-#[inline]
-fn gcd(a: u128, b: u128) -> u128 {
-    let mut max = std::cmp::max(a, b);
-    let mut min = std::cmp::min(a, b);
-    while min != 0 {
-        let tmp = min;
-        min = max % min;
-        max = tmp;
+pub mod number_library {
+    #[inline]
+    pub fn gcd<T>(a: T, b: T) -> T
+    where
+        T: std::ops::Rem<Output = T> + Zero + std::cmp::Eq + Copy + std::cmp::Ord,
+    {
+        let mut max = std::cmp::max(a, b);
+        let mut min = std::cmp::min(a, b);
+        while min != T::ZERO {
+            let tmp = min;
+            min = max % min;
+            max = tmp;
+        }
+        max
     }
-    max
-}
 
-#[inline]
-fn lcm(a: u128, b: u128) -> u128 {
-    let gcd = gcd(a, b);
-    a / gcd * b
+    #[inline]
+    pub fn lcm<T>(a: T, b: T) -> T
+    where
+        T: Copy
+            + std::ops::Rem<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Div<Output = T>
+            + std::cmp::Ord
+            + Zero
+            + std::cmp::Eq,
+    {
+        a / gcd(a, b) * b
+    }
+
+    pub trait Zero {
+        const ZERO: Self;
+    }
+
+    macro_rules! impl_zero {
+            ( $($e:ty),* ) => {
+                $(
+                    impl Zero for $e {
+                        const ZERO: Self = 0;
+                    }
+                )*
+            };
+        }
+
+    impl_zero!(isize, i8, i16, i32, i64, i128, usize, u8, u16, u32, u64, u128);
+
+    #[cfg(test)]
+    mod tests_number {
+        use super::*;
+
+        #[test]
+        fn for_gcd() {
+            assert_eq!(gcd(9, 12), 3);
+        }
+
+        #[test]
+        fn for_lcm() {
+            assert_eq!(lcm(9, 12), 36);
+        }
+    }
 }
