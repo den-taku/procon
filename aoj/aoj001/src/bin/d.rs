e8fa00320b4e67f21ee23f157ef0de757a507e7a
dentaku <dentaku1412@gmail.com>
2021-09-14 20:56:15 +0900

    AC

--- aoj/aoj001/src/bin/d.rs
+++ aoj/aoj001/src/bin/d.rs
@@ -4,7 +4,7 @@
 fn main() {
     while let Some(v) = input() {
         let mut ans = 0;
-        for e in primes_library::Prime::<usize>::new() {
+        for e in primes_library::Seive::<usize>::new() {
             if e > v {
                 break;
             } else {
@@ -40,61 +40,101 @@ where
 }
 
 pub mod primes_library {
-    macro_rules! impl_num {
-        ( $($e:ty),* ) => {
-            $(
-                impl Prime<$e> {
-                    impl_new!($e);
-                }
-
-                impl Iterator for Prime<$e> {
-                    impl_next!($e);
-                }
-            )*
-        };
+    // verified (https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5878093#1)
+    pub struct Seive<T> {
+        iter: Box<std::iter::Chain<std::ops::Range<T>, P<T>>>,
     }
 
-    macro_rules! impl_new {
-        ($e:ty) => {
-            #[inline]
-            pub fn new() -> Self {
-                Self {
-                    iterator: Box::new(2..),
-                    primes: Vec::new(),
-                }
-            }
-        };
+    struct P<T> {
+        dict: std::collections::HashMap<T, T>,
+        iter: Box<std::ops::RangeFrom<T>>,
     }
 
-    macro_rules! impl_next {
-        ($e:ty) => {
-            type Item = $e;
-            #[inline]
-            fn next(&mut self) -> Option<Self::Item> {
-                while let Some(e) = self.iterator.next() {
-                    if self.primes.iter().all(|&p| e % p != 0) {
-                        self.primes.push(e);
-                        return Some(e);
-                    }
-                }
-                None
-            }
-        };
-    }
-
-    pub struct Prime<T> {
-        iterator: Box<dyn Iterator<Item = T>>,
-        primes: Vec<T>,
-    }
-
-    impl Default for Prime<i32> {
+    impl Default for Seive<i32> {
         #[inline]
         fn default() -> Self {
             Self::new()
         }
     }
 
+    macro_rules! impl_num {
+        ( $($e:ty), *) => {
+            $(
+                impl Seive<$e> {
+                    pub fn new() -> Self {
+                        Self {
+                            iter: Box::new((2..3).chain(P::<$e>::new()))
+                        }
+                    }
+                }
+
+                impl Iterator for Seive<$e> {
+                    type Item = $e;
+                    #[inline]
+                    fn next(&mut self) -> Option<Self::Item> {
+                        self.iter.next()
+                    }
+                }
+
+                impl P<$e> {
+                    fn new() -> Self {
+                        Self {
+                            dict: std::collections::HashMap::new(),
+                            iter: Box::new((1..))
+                        }
+                    }
+                }
+
+                impl Iterator for P<$e> {
+                    type Item = $e;
+                    #[inline]
+                    fn next(&mut self) -> Option<Self::Item> {
+                        while let Some(j) = self.iter.next() {
+                            let i = 2 * j + 1;
+                            let (factor, is_prime) = match self.dict.remove(&i) {
+                                Some(f) => (f, false),
+                                None => (i * 2, true),
+                            };
+                            let key = (1..)
+                                .filter_map(|j| {
+                                    let k = i + j * factor;
+                                    if self.dict.contains_key(&k) {
+                                        None
+                                    } else {
+                                        Some(k)
+                                    }
+                                })
+                                .next()
+                                .unwrap();
+                            self.dict.insert(key, factor);
+                            if is_prime {
+                                return Some(i);
+                            }
+                        }
+                    None
+                    }
+                }
+            )*
+        };
+    }
+
     impl_num!(isize, i32, i64, i128, usize, u32, u64, u128);
+
+    pub trait Two {
+        const TWO: Self;
+    }
+
+    macro_rules! impl_two {
+        ( $($e:ty),* ) => {
+            $(
+                impl Two for $e {
+                    const TWO: Self = 2;
+                }
+            )*
+        };
+    }
+
+    impl_two!(isize, i8, i16, i32, i64, i128, usize, u8, u16, u32, u64, u128);
 
     #[cfg(test)]
     mod tests_primes {
@@ -102,24 +142,24 @@ pub mod primes_library {
 
         #[test]
         fn for_primes() {
-            assert_eq!(Prime::<isize>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<usize>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<i32>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<u32>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<i64>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<u64>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<i128>::new().take(100).last(), Some(541));
-            assert_eq!(Prime::<u128>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<isize>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<usize>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<i32>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<u32>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<i64>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<u64>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<i128>::new().take(100).last(), Some(541));
+            assert_eq!(Seive::<u128>::new().take(100).last(), Some(541));
 
-            assert_eq!(Prime::default().take(10_000).last(), Some(104_729));
+            assert_eq!(Seive::default().take(10_000).last(), Some(104_729));
 
             assert_eq!(
-                Prime::default().take_while(|&e| e < 200_000).last(),
+                Seive::default().take_while(|&e| e < 200_000).last(),
                 Some(199_999)
             );
 
             assert_eq!(
-                Prime::default().take_while(|&e| e < 20).collect::<Vec<_>>(),
+                Seive::default().take_while(|&e| e < 20).collect::<Vec<_>>(),
                 vec![2, 3, 5, 7, 11, 13, 17, 19]
             )
         }
