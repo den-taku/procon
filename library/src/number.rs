5888ec7bc8da0bbf727989a307ab37698a770eb8
dentaku <dentaku1412@gmail.com>
2021-09-14 17:43:12 +0900

    Implement ext_gcd

--- library/src/number.rs
+++ library/src/number.rs
@@ -30,6 +30,28 @@ pub mod number_library {
         a / gcd(a, b) * b
     }
 
+    #[inline]
+    /// return gcd(a, b), x, y s.t. ax + by = gcd(a, b)
+    /// verified (https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5877610#1)
+    pub fn ext_gcd<T>(a: T, b: T) -> (T, T, T)
+    where
+        T: std::ops::Rem<Output = T>
+            + std::ops::Div<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Sub<Output = T>
+            + Zero
+            + One
+            + std::cmp::Eq
+            + Copy,
+    {
+        if b == T::ZERO {
+            (a, T::ONE, T::ZERO)
+        } else {
+            let (d, y_b, x_b) = ext_gcd(b, a % b);
+            (d, x_b, y_b - (a / b) * x_b)
+        }
+    }
+
     pub trait Zero {
         const ZERO: Self;
     }
@@ -46,6 +68,22 @@ pub mod number_library {
 
     impl_zero!(isize, i8, i16, i32, i64, i128, usize, u8, u16, u32, u64, u128);
 
+    pub trait One {
+        const ONE: Self;
+    }
+
+    macro_rules! impl_one {
+            ( $($e:ty),* ) => {
+                $(
+                    impl One for $e {
+                        const ONE: Self = 1;
+                    }
+                )*
+            };
+        }
+
+    impl_one!(isize, i8, i16, i32, i64, i128, usize, u8, u16, u32, u64, u128);
+
     #[cfg(test)]
     mod tests_number {
         use super::*;
@@ -58,6 +96,12 @@ pub mod number_library {
         #[test]
         fn for_lcm() {
             assert_eq!(lcm(9, 12), 36);
+        }
+
+        #[test]
+        fn for_ext_gcd() {
+            assert_eq!(ext_gcd(4, 12), (4, 1, 0));
+            assert_eq!(ext_gcd(3, 8), (1, 3, -1));
         }
     }
 }
