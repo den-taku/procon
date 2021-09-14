2a6734f4d8ea4b6b70ac528e0102c67f6df03168
dentaku <dentaku1412@gmail.com>
2021-09-14 17:43:31 +0900

    AC Extended Euclid Algorithm

--- aoj/aoj001/src/bin/e.rs
+++ aoj/aoj001/src/bin/e.rs
@@ -1,22 +1,14 @@
+// Extended Euclid Algorithm (https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_E&lang=ja)
 #![allow(unreachable_code)]
-//use proconio::{fastout, input};
+#[allow(dead_code)]
 
-// #[fastout]
 fn main() {
-    // input! {
-    //     n: usize,
-    //     t: [[i128; 50];50]
-    // }
-    let n = read_line::<usize>()[0];
-    let mut t = Vec::with_capacity(n);
-    let _ = (0..n)
-        .map(|_| {
-            let elem = read_line::<i64>();
-            t.push(elem);
-        })
-        .collect::<Vec<()>>();
-    let _t = t;
-    unimplemented!()
+    let (a, b) = {
+        let e = read_line::<i64>();
+        (e[0], e[1])
+    };
+    let (_, x, y) = number_library::ext_gcd(a, b);
+    println!("{} {}", x, y);
 }
 
 #[inline]
@@ -31,4 +23,103 @@ where
         .split_whitespace()
         .map(|c| T::from_str(c).unwrap())
         .collect()
+}
+
+pub mod number_library {
+    #[inline]
+    pub fn gcd<T>(a: T, b: T) -> T
+    where
+        T: std::ops::Rem<Output = T> + Zero + std::cmp::Eq + Copy + std::cmp::Ord,
+    {
+        let mut max = std::cmp::max(a, b);
+        let mut min = std::cmp::min(a, b);
+        while min != T::ZERO {
+            let tmp = min;
+            min = max % min;
+            max = tmp;
+        }
+        max
+    }
+
+    #[inline]
+    pub fn lcm<T>(a: T, b: T) -> T
+    where
+        T: Copy
+            + std::ops::Rem<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Div<Output = T>
+            + std::cmp::Ord
+            + Zero
+            + std::cmp::Eq,
+    {
+        a / gcd(a, b) * b
+    }
+
+    #[inline]
+    /// return gcd(a, b), x, y s.t. ax + by = gcd(a, b)
+    pub fn ext_gcd<T>(a: T, b: T) -> (T, T, T)
+    where
+        T: std::ops::Rem<Output = T>
+            + std::ops::Div<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::Sub<Output = T>
+            + Zero
+            + One
+            + std::cmp::Eq
+            + Copy,
+    {
+        if b == T::ZERO {
+            (a, T::ONE, T::ZERO)
+        } else {
+            let (d, y_b, x_b) = ext_gcd(b, a % b);
+            (d, x_b, y_b - (a / b) * x_b)
+        }
+    }
+
+    pub trait Zero {
+        const ZERO: Self;
+    }
+
+    macro_rules! impl_zero {
+            ( $($e:ty),* ) => {
+                $(
+                    impl Zero for $e {
+                        const ZERO: Self = 0;
+                    }
+                )*
+            };
+        }
+
+    impl_zero!(isize, i8, i16, i32, i64, i128, usize, u8, u16, u32, u64, u128);
+
+    pub trait One {
+        const ONE: Self;
+    }
+
+    macro_rules! impl_one {
+            ( $($e:ty),* ) => {
+                $(
+                    impl One for $e {
+                        const ONE: Self = 1;
+                    }
+                )*
+            };
+        }
+
+    impl_one!(isize, i8, i16, i32, i64, i128, usize, u8, u16, u32, u64, u128);
+
+    #[cfg(test)]
+    mod tests_number {
+        use super::*;
+
+        #[test]
+        fn for_gcd() {
+            assert_eq!(gcd(9, 12), 3);
+        }
+
+        #[test]
+        fn for_lcm() {
+            assert_eq!(lcm(9, 12), 36);
+        }
+    }
 }
