cc44a3138d29f12d9e60f791669030a75c1cf6de
dentaku <dentaku1412@gmail.com>
2021-09-19 14:47:47 +0900

    AC ARC119 C

--- atcoder/arc/arc119/src/bin/c.rs
+++ atcoder/arc/arc119/src/bin/c.rs
@@ -4,8 +4,27 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        a: [i128; n]
     }
-    unimplemented!()
+    let mut sum = vec![0; n + 1];
+    for i in 1..n + 1 {
+        if i % 2 != 0 {
+            sum[i] += a[i - 1]
+        } else {
+            sum[i] -= a[i - 1]
+        }
+        sum[i] += sum[i - 1];
+    }
+    let mut map = std::collections::HashMap::new();
+    for s in sum {
+        *map.entry(s).or_insert(0) += 1;
+    }
+    println!(
+        "{}",
+        map.iter()
+            .map(|e| e.1)
+            .map(|&e| if e >= 2 { e * (e - 1) / 2 } else { 0 })
+            .sum::<i128>()
+    )
 }
