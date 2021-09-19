f59d8ae4fbbef90d1fc9226b15d72655d1f8b03e
dentaku <dentaku1412@gmail.com>
2021-09-19 13:55:26 +0900

    AC ARC119 B

--- atcoder/arc/arc119/src/bin/b.rs
+++ atcoder/arc/arc119/src/bin/b.rs
@@ -4,8 +4,35 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        s: String,
+        t: String
     }
-    unimplemented!()
+    if s.chars().filter(|&c| c == '1').count() != t.chars().filter(|&c| c == '1').count() {
+        println!("-1");
+        return;
+    }
+    let mut s = s
+        .chars()
+        .map(|c| if c == '1' { 1 } else { 0 })
+        .collect::<Vec<_>>();
+    let mut t = t
+        .chars()
+        .map(|c| if c == '1' { 1 } else { 0 })
+        .collect::<Vec<_>>();
+    let mut ans = 0;
+    for i in 0..n {
+        if s[i] == t[i] {
+            continue;
+        }
+        if s[i] == 0 {
+            let index = t.iter().enumerate().skip(i).find(|e| e.1 == &0).unwrap().0;
+            t.swap(i, index);
+        } else {
+            let index = s.iter().enumerate().skip(i).find(|e| e.1 == &0).unwrap().0;
+            s.swap(i, index);
+        }
+        ans += 1;
+    }
+    println!("{}", ans);
 }
