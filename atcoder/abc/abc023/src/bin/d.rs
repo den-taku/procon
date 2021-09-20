d395f070bebb70f1f8880cd90c961b7a92bc03db
dentaku <dentaku1412@gmail.com>
2021-09-20 17:38:39 +0900

    AC ABC023 D

--- atcoder/abc/abc023/src/bin/d.rs
+++ atcoder/abc/abc023/src/bin/d.rs
@@ -4,8 +4,41 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        hs: [(usize, usize); n]
     }
-    unimplemented!()
+    let max = hs.iter().map(|(h, s)| h + s * hs.len()).max().unwrap();
+    let mut ub = max + 3;
+    let mut lb = 1;
+    while ub - lb > 1 {
+        let est = (ub + lb) / 2;
+        if condition(est, &hs) {
+            ub = est;
+        } else {
+            lb = est;
+        }
+    }
+    if condition(lb, &hs) {
+        println!("{}", lb)
+    } else {
+        println!("{}", ub)
+    }
+}
+
+#[inline(always)]
+fn condition(est: usize, hs: &[(usize, usize)]) -> bool {
+    let mut v = vec![];
+    for (h, s) in hs {
+        if &est < h {
+            return false;
+        }
+        v.push((est - h) / s)
+    }
+    v.sort();
+    for (i, &e) in v.iter().enumerate() {
+        if i > e {
+            return false;
+        }
+    }
+    true
 }
