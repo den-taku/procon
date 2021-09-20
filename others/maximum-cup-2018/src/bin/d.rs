8d4cb0468cb4cc1047ea6928cd6312bf16dbd83a
dentaku <dentaku1412@gmail.com>
2021-09-20 16:20:11 +0900

    AC Maximum Cup 2018 D

--- others/maximum-cup-2018/src/bin/d.rs
+++ others/maximum-cup-2018/src/bin/d.rs
@@ -4,8 +4,34 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        l: usize,
+        x: usize,
+        a: [usize; n]
     }
-    unimplemented!()
+    let mut dp = vec![None; m];
+    dp[0] = Some(0);
+    for e in a {
+        let mut next = dp.clone();
+        for i in 0..m {
+            if let Some(c) = dp[i] {
+                let over = (i + e) / m;
+                if c + over >= x {
+                    continue;
+                }
+                if let Some(v) = dp[(i + e) % m] {
+                    next[(i + e) % m] = Some(std::cmp::min(v, c + over))
+                } else {
+                    next[(i + e) % m] = Some(c + over)
+                }
+            }
+        }
+        dp = next;
+    }
+    if dp[l].is_some() {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
