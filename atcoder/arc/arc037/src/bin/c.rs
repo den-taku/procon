556291158683ba18bc6f6de4d1f97b032ce79fa6
dentaku <dentaku1412@gmail.com>
2021-09-20 16:43:44 +0900

    Mendokusai...

--- atcoder/arc/arc037/src/bin/c.rs
+++ atcoder/arc/arc037/src/bin/c.rs
@@ -1,11 +1,27 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::collections::BinaryHeap;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        k: usize,
+        ai: [usize; n],
+        bj: [usize; n]
     }
-    unimplemented!()
+    let mut heap = BinaryHeap::new();
+    for &a in ai.iter() {
+        for &b in bj.iter() {
+            if heap.len() < k {
+                heap.push(a * b);
+            } else {
+                if *heap.peek().unwrap() > a * b {
+                    heap.pop();
+                    heap.push(a * b);
+                }
+            }
+        }
+    }
+    println!("{}", heap.peek().unwrap());
 }
