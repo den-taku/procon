443f2c7449900559d036f282f34a0ba3dace0ef6
dentaku <dentaku1412@gmail.com>
2021-09-22 23:56:15 +0900

    AC ARC034 D

--- atcoder/abc/abc034/src/bin/d.rs
+++ atcoder/abc/abc034/src/bin/d.rs
@@ -4,8 +4,30 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        k: usize,
+        wp: [(f64, f64); n]
     }
-    unimplemented!()
+    let mut ub = 100.0;
+    let mut lb = 0.0;
+    while ub - lb > 1e-5 {
+        let est = (ub + lb) / 2.0;
+        if condition(&wp, est, k) {
+            lb = est
+        } else {
+            ub = est
+        }
+    }
+    if condition(&wp, ub, k) {
+        println!("{}", ub)
+    } else {
+        println!("{}", lb)
+    }
+}
+
+#[inline(always)]
+fn condition(wp: &[(f64, f64)], est: f64, k: usize) -> bool {
+    let mut array = wp.iter().map(|&(w, p)| w * p - est * w).collect::<Vec<_>>();
+    array.sort_by(|a, b| b.partial_cmp(a).unwrap());
+    array.iter().take(k).sum::<f64>() > 0.0
 }
