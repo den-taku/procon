554a4a1ab64179df4c2595f09ae9c922ae8f366e
dentaku <dentaku1412@gmail.com>
2021-09-23 00:58:28 +0900

    AC ABC038 C

--- atcoder/abc/abc038/src/bin/c.rs
+++ atcoder/abc/abc038/src/bin/c.rs
@@ -4,8 +4,19 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        array: [usize; n]
     }
-    unimplemented!()
+    let mut sum = 1u64;
+    let mut count = 1;
+    for (a, b) in array.iter().zip(array.iter().skip(1)) {
+        if a < b {
+            count += 1;
+            sum += count;
+        } else {
+            count = 1;
+            sum += count;
+        }
+    }
+    println!("{}", sum);
 }
