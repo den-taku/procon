94e89456cd335d13e33a3c00d100a1dfd9042c83
dentaku <dentaku1412@gmail.com>
2021-09-26 20:35:08 +0900

    AC ABC118 C

--- atcoder/abc/abc118/src/bin/c.rs
+++ atcoder/abc/abc118/src/bin/c.rs
@@ -1,11 +1,22 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::collections::BTreeSet;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        a: [usize; n]
     }
-    unimplemented!()
+    let mut a = a.into_iter().collect::<BTreeSet<_>>();
+    a.insert(0);
+    while a.len() > 2 {
+        let &one = a.iter().last().unwrap();
+        a.remove(&one);
+        let &two = a.iter().last().unwrap();
+        a.insert(one % two);
+    }
+    for e in a.iter().last() {
+        println!("{}", e)
+    }
 }
