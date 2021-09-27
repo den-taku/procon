1f525a6d727b7688e07f89917a56afc5bd8f79ec
dentaku <dentaku1412@gmail.com>
2021-09-28 04:53:59 +0900

    Implement solver for simultaneous linear congruent

--- library/src/number.rs
+++ library/src/number.rs
@@ -56,12 +56,13 @@ pub mod number_library {
     }
 
     /// return inverse element a^-1 s.t. a * a^-1 ≡ 1 (mod m)
-    /// verified (https://atcoder.jp/contests/abc024/submissions/26192341)
+    /// verified (https://atcoder.jp/contests/abc024/submissions/26198316)
     /// O(lg n)
     #[inline]
     pub fn mod_inverse<T>(a: T, m: T) -> Option<T>
     where
         T: std::ops::Rem<Output = T>
+            + std::ops::Add<Output = T>
             + std::ops::Div<Output = T>
             + std::ops::Mul<Output = T>
             + std::ops::Sub<Output = T>
@@ -72,10 +73,53 @@ pub mod number_library {
     {
         let (d, x, _) = ext_gcd(a, m);
         if d == T::ONE {
-            Some(x)
+            Some((m + x % m) % m)
         } else {
             None
         }
+    }
+
+    /// solve simultaneous linear congruent
+    /// give ai x ≡ bi (mod mi) (1 <= i <= n) as Vec<(ai, bi, mi)>,
+    /// then return Option<(b, m)> s.t. x ≡ b (mod m)
+    #[inline]
+    pub fn solve_simultaneous_linear_congruent<T>(variables: &[(T, T, T)]) -> Option<(T, T)>
+    where
+        T: Zero
+            + One
+            + Copy
+            + std::cmp::Eq
+            + std::cmp::Ord
+            + std::ops::Add<Output = T>
+            + std::ops::Sub<Output = T>
+            + std::ops::Mul<Output = T>
+            + std::ops::MulAssign
+            + std::ops::Div<Output = T>
+            + std::ops::Rem<Output = T>,
+    {
+        // ∀x, x ≡ 0 (mod 1)
+        let mut x = T::ZERO;
+        let mut m1 = T::ONE;
+        for &(a, b, m2) in variables {
+            let am1 = a * m1;
+            let b2_ab1 = {
+                let tmp = (b - a * x) % m2;
+                if tmp < T::ZERO {
+                    tmp + m2
+                } else {
+                    tmp
+                }
+            };
+            let d = gcd(am1, m2);
+            if b2_ab1 % d != T::ZERO {
+                // no answer exists.
+                return None;
+            }
+            let t = b2_ab1 / d * mod_inverse(am1 / d, m2 / d).unwrap() % (m2 / d);
+            x = x + m1 * t;
+            m1 *= m2 / d;
+        }
+        Some((x % m1, m1))
     }
 
     pub trait Zero {
@@ -134,6 +178,19 @@ pub mod number_library {
         fn for_mod_inverse() {
             assert_eq!(mod_inverse(4, 11), Some(3));
             assert_eq!(mod_inverse(7, 13), Some(2));
+        }
+
+        #[test]
+        fn for_solve_simultaneous() {
+            let problems = vec![
+                vec![(1, 10, 20), (1, 10, 30), (1, 10, 40)],
+                vec![(1, 10, 20), (1, 10, 30), (1, 30, 40)],
+                vec![(1, 80712, 221549), (1, 320302, 699312), (1, 140367, 496729)],
+            ];
+            let answers = vec![10, 70, 38774484298448350i64];
+            for (p, a) in problems.iter().zip(answers) {
+                assert_eq!(solve_simultaneous_linear_congruent(p).unwrap().0, a);
+            }
         }
     }
 }
