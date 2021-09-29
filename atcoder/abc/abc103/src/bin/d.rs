aaa792200d98b1d74eae0bfb37d2d71db9cd8ede
dentaku <dentaku1412@gmail.com>
2021-09-29 20:54:13 +0900

    AC ABC103 D

--- atcoder/abc/abc103/src/bin/d.rs
+++ atcoder/abc/abc103/src/bin/d.rs
@@ -4,8 +4,29 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        queries: [(usize, usize); m]
     }
-    unimplemented!()
+    let mut v = vec![None; n];
+    for &(a, b) in &queries {
+        if let Some(start) = v[b - 1] {
+            if start < a {
+                v[b - 1] = Some(a)
+            }
+        } else {
+            v[b - 1] = Some(a)
+        }
+    }
+    let mut count = 0;
+    let mut index = 0;
+    for (i, &e) in v.iter().enumerate() {
+        if let Some(start) = e {
+            if start >= index {
+                count += 1;
+                index = i + 1;
+            }
+        }
+    }
+    println!("{}", count);
 }
