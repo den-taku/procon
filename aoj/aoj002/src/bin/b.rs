7deb7922a23b1b9f7e2bd2900e4db27d3c61421f
dentaku <dentaku1412@gmail.com>
2021-10-05 20:45:51 +0900

    AC RMQ

--- aoj/aoj002/src/bin/b.rs
+++ aoj/aoj002/src/bin/b.rs
@@ -1,22 +1,31 @@
 #![allow(unreachable_code)]
-//use proconio::{fastout, input};
+use segment_tree_library::*;
 
-// #[fastout]
 fn main() {
-    // input! {
-    //     n: usize,
-    //     t: [[i128; 50];50]
-    // }
-    let n = read_line::<usize>()[0];
-    let mut t = Vec::with_capacity(n);
-    let _ = (0..n)
-        .map(|_| {
-            let elem = read_line::<i64>();
-            t.push(elem);
-        })
-        .collect::<Vec<()>>();
-    let _t = t;
-    unimplemented!()
+    let condition = read_line::<usize>();
+    let n = condition[0];
+    let q = condition[1];
+    let mut com = Vec::with_capacity(q);
+    for _ in 0..q {
+        let elem = read_line::<usize>();
+        com.push((elem[0], elem[1], elem[2]));
+    }
+    let com = com;
+
+    let mut segment = SegmentTree::new(n, std::cmp::min, std::i64::MAX);
+    for &(d, x, y) in &com {
+        match d {
+            0 => segment.update(x, y as i64),
+            1 => println!(
+                "{}",
+                match segment.find(x..y + 1) {
+                    std::i64::MAX => 2_147_483_647i64,
+                    a => a,
+                }
+            ),
+            _ => unreachable!(),
+        }
+    }
 }
 
 #[inline]
@@ -31,4 +40,130 @@ where
         .split_whitespace()
         .map(|c| T::from_str(c).unwrap())
         .collect()
+}
+
+pub mod segment_tree_library {
+    #[derive(Debug)]
+    pub struct SegmentTree<T, F>
+    where
+        F: Fn(T, T) -> T,
+    {
+        tree: Vec<T>,
+        n: usize,
+        f: F,
+        unit: T,
+    }
+
+    impl<T, F> SegmentTree<T, F>
+    where
+        T: Copy,
+        F: Fn(T, T) -> T,
+    {
+        #[inline]
+        pub fn new(n: usize, f: F, unit: T) -> Self {
+            let mut n_ = 1;
+            while n_ < n {
+                n_ *= 2
+            }
+            Self {
+                tree: vec![unit; n_ * 2 - 1],
+                n: n_,
+                f,
+                unit,
+            }
+        }
+
+        #[inline]
+        pub fn update(&mut self, index: usize, value: T) {
+            if index >= self.n {
+                panic!("SegmentTree: out of bound")
+            }
+            let mut k = self.n - 1 + index;
+            self[k] = value;
+            while k > 0 {
+                k = (k - 1) / 2;
+                self[k] = (self.f)(self[k * 2 + 1], self[k * 2 + 2])
+            }
+        }
+
+        #[inline]
+        pub fn find(&self, range: std::ops::Range<usize>) -> T {
+            self.find_rec(range, 0, 0, self.n)
+        }
+
+        fn find_rec(&self, range: std::ops::Range<usize>, k: usize, l: usize, r: usize) -> T {
+            if r <= range.start || range.end <= l {
+                self.unit
+            } else if range.start <= l && r <= range.end {
+                self[k]
+            } else {
+                (self.f)(
+                    self.find_rec(range.clone(), k * 2 + 1, l, (l + r) / 2),
+                    self.find_rec(range, k * 2 + 2, (l + r) / 2, r),
+                )
+            }
+        }
+    }
+
+    impl<T, F> std::ops::Index<usize> for SegmentTree<T, F>
+    where
+        F: Fn(T, T) -> T,
+    {
+        type Output = T;
+        #[inline]
+        fn index(&self, index: usize) -> &Self::Output {
+            &self.tree[index]
+        }
+    }
+
+    impl<T, F> std::ops::IndexMut<usize> for SegmentTree<T, F>
+    where
+        F: Fn(T, T) -> T,
+    {
+        #[inline]
+        fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+            &mut self.tree[index]
+        }
+    }
+
+    #[cfg(test)]
+    mod tests_segment_tree {
+        use super::*;
+
+        #[test]
+        fn for_rmq1() {
+            let n = 3;
+            let _q = 5;
+            let com = [(0, 0, 1), (0, 1, 2), (0, 2, 3), (1, 0, 2), (1, 1, 2)];
+            let answers = &[1, 2];
+            let mut rmq = SegmentTree::new(n, std::cmp::min, std::usize::MAX);
+            let mut i = 0;
+            for &(d, x, y) in &com {
+                match d {
+                    0 => rmq.update(x, y),
+                    1 => {
+                        assert_eq!(rmq.find(x..y + 1), answers[i]);
+                        i += 1;
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            let n = 1;
+            let _q = 3;
+            let com = [(1, 0, 0), (0, 0, 5), (1, 0, 0)];
+            let answers = &[std::usize::MAX, 5];
+            let mut rmq = SegmentTree::new(n, std::cmp::min, std::usize::MAX);
+            let mut i = 0;
+            for &(d, x, y) in &com {
+                match d {
+                    0 => rmq.update(x, y),
+                    1 => {
+                        assert_eq!(rmq.find(x..y + 1), answers[i]);
+                        i += 1;
+                    }
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
 }
