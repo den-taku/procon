9731d891a0663803018f83b0b704ea2c055f6ba7
dentaku <dentaku1412@gmail.com>
2021-10-06 01:05:15 +0900

    Implement DeleydSegmentTree

--- library/src/segment_tree_delay.rs
+++ library/src/segment_tree_delay.rs
@@ -1,28 +1,40 @@
 #![allow(dead_code)]
 
 pub mod segment_tree_delay_library {
-    /// TODO: Implement
-    /// Segment Tree for Monoid (T, F)
+    /// Delayed Segment Tree for M-act Monoid
+    /// (T, F) : Monoid
+    /// (U, G) : Monoid
+    /// act: T × U -> T, isomorphic.
+    /// (xy) act m = (x act m) (y act m)
+    /// x act (mn) = (x act m) (x act n)
+    ///
+    /// In this implementation, Act is ^, then return a_s * ... a_t * m ^ range
+    /// unit of U is 0 scinse U is usize
+    ///
+    /// verified (https://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=5940499#1)
     #[derive(Debug)]
-    pub struct DelaySegmentTree<T, F>
+    pub struct DelayedSegmentTree<T, F, Act>
     where
         F: Fn(T, T) -> T,
+        Act: Fn(T, usize) -> T,
     {
         tree: Vec<T>,
         tree_range: Vec<T>,
         n: usize,
         f: F,
         unit: T,
+        act: Act,
     }
 
-    impl<T, F> DelaySegmentTree<T, F>
+    impl<T, F, Act> DelayedSegmentTree<T, F, Act>
     where
         T: Copy,
         F: Fn(T, T) -> T,
+        Act: Fn(T, usize) -> T,
     {
         /// O(n)
         #[inline]
-        pub fn new(n: usize, f: F, unit: T) -> Self {
+        pub fn new(n: usize, f: F, unit: T, act: Act) -> Self {
             let mut n_ = 1;
             while n_ < n {
                 n_ *= 2
@@ -33,6 +45,7 @@ pub mod segment_tree_delay_library {
                 n: n_,
                 f,
                 unit,
+                act,
             }
         }
 
@@ -41,13 +54,45 @@ pub mod segment_tree_delay_library {
         #[inline]
         pub fn update(&mut self, index: usize, value: T) {
             if index >= self.n {
-                panic!("SegmentTree: out of bound")
+                panic!("DelayedSegmentTree: out of bound")
             }
             let mut k = self.n - 1 + index;
             self[k] = value;
             while k > 0 {
                 k = (k - 1) / 2;
                 self[k] = (self.f)(self[k * 2 + 1], self[k * 2 + 2])
+            }
+        }
+
+        /// act in range [a, b)
+        #[inline]
+        pub fn act_in_range(&mut self, range: std::ops::Range<usize>, value: T) {
+            if range.end > self.n {
+                panic!("DelayedSegmentTree: out of bound")
+            }
+            self.act_in_range_rec(range, value, 0, 0, self.n)
+        }
+
+        fn act_in_range_rec(
+            &mut self,
+            range: std::ops::Range<usize>,
+            value: T,
+            k: usize,
+            l: usize,
+            r: usize,
+        ) {
+            if range.start <= l && r <= range.end {
+                self.tree_range[k] = (self.f)(self.tree_range[k], value)
+            } else if l < range.end && range.start < r {
+                self[k] = (self.f)(
+                    self[k],
+                    (self.act)(
+                        value,
+                        std::cmp::min(range.end, r) - std::cmp::max(range.start, l),
+                    ),
+                );
+                self.act_in_range_rec(range.clone(), value, k * 2 + 1, l, (l + r) / 2);
+                self.act_in_range_rec(range, value, k * 2 + 2, (l + r) / 2, r);
             }
         }
 
@@ -62,19 +107,26 @@ pub mod segment_tree_delay_library {
             if r <= range.start || range.end <= l {
                 self.unit
             } else if range.start <= l && r <= range.end {
-                self[k]
+                (self.f)(self[k], (self.act)(self.tree_range[k], r - l))
             } else {
                 (self.f)(
-                    self.find_rec(range.clone(), k * 2 + 1, l, (l + r) / 2),
-                    self.find_rec(range, k * 2 + 2, (l + r) / 2, r),
+                    (self.act)(
+                        self.tree_range[k],
+                        std::cmp::min(range.end, r) - std::cmp::max(range.start, l),
+                    ),
+                    (self.f)(
+                        self.find_rec(range.clone(), k * 2 + 1, l, (l + r) / 2),
+                        self.find_rec(range, k * 2 + 2, (l + r) / 2, r),
+                    ),
                 )
             }
         }
     }
 
-    impl<T, F> std::ops::Index<usize> for DelaySegmentTree<T, F>
+    impl<T, F, Act> std::ops::Index<usize> for DelayedSegmentTree<T, F, Act>
     where
         F: Fn(T, T) -> T,
+        Act: Fn(T, usize) -> T,
     {
         type Output = T;
         #[inline]
@@ -83,9 +135,10 @@ pub mod segment_tree_delay_library {
         }
     }
 
-    impl<T, F> std::ops::IndexMut<usize> for DelaySegmentTree<T, F>
+    impl<T, F, Act> std::ops::IndexMut<usize> for DelayedSegmentTree<T, F, Act>
     where
         F: Fn(T, T) -> T,
+        Act: Fn(T, usize) -> T,
     {
         #[inline]
         fn index_mut(&mut self, index: usize) -> &mut Self::Output {
@@ -103,7 +156,7 @@ pub mod segment_tree_delay_library {
             let _q = 5;
             let com = [(0, 0, 1), (0, 1, 2), (0, 2, 3), (1, 0, 2), (1, 1, 2)];
             let answers = &[1, 2];
-            let mut rmq = DelaySegmentTree::new(n, std::cmp::min, std::usize::MAX);
+            let mut rmq = DelayedSegmentTree::new(n, std::cmp::min, std::usize::MAX, |a, b| a * b);
             let mut i = 0;
             for &(d, x, y) in &com {
                 match d {
@@ -119,7 +172,7 @@ pub mod segment_tree_delay_library {
             let _q = 3;
             let com = [(1, 0, 0), (0, 0, 5), (1, 0, 0)];
             let answers = &[std::usize::MAX, 5];
-            let mut rmq = DelaySegmentTree::new(n, std::cmp::min, std::usize::MAX);
+            let mut rmq = DelayedSegmentTree::new(n, std::cmp::min, std::usize::MAX, |a, b| a * b);
             let mut i = 0;
             for &(d, x, y) in &com {
                 match d {
@@ -129,6 +182,34 @@ pub mod segment_tree_delay_library {
                         i += 1;
                     }
                     _ => unreachable!(),
+                }
+            }
+        }
+
+        #[test]
+        fn for_rsq_and_raq() {
+            let ns = vec![3, 4];
+            let queriess = vec![
+                vec![
+                    vec![0, 1, 2, 1],
+                    vec![0, 2, 3, 2],
+                    vec![0, 3, 3, 3],
+                    vec![1, 1, 2],
+                    vec![1, 2, 3],
+                ],
+                vec![vec![1, 1, 4], vec![0, 1, 4, 1], vec![1, 1, 4]],
+            ];
+            let answers = vec![4, 8, 0, 4];
+            let mut index = 0;
+            for (n, queries) in ns.into_iter().zip(queriess.into_iter()) {
+                let mut tree = DelayedSegmentTree::new(n, |a, b| a + b, 0, |a, b| a * b);
+                for query in queries {
+                    if query[0] == 0 {
+                        tree.act_in_range(query[1] - 1..query[2], query[3])
+                    } else {
+                        assert_eq!(tree.find(query[1] - 1..query[2]), answers[index]);
+                        index += 1;
+                    }
                 }
             }
         }
