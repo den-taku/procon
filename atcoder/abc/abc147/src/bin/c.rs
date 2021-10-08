bacac7422ba4b32b561095fcfc81e997885089b7
dentaku <dentaku1412@gmail.com>
2021-10-08 15:52:19 +0900

    AC ABC147 C

--- atcoder/abc/abc147/src/bin/c.rs
+++ atcoder/abc/abc147/src/bin/c.rs
@@ -1,11 +1,59 @@
+#![allow(dead_code)]
 #![allow(unreachable_code)]
-use proconio::{fastout, input};
 
-#[fastout]
 fn main() {
-    input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+    let input = input();
+    let n = input.len();
+    let mut ans = 0;
+    'out: for set in 1..1 << n {
+        let mut people = vec![false; n];
+        let mut count = 0;
+        for i in 0..n {
+            if set >> i & 1 == 1 {
+                count += 1;
+                people[i] = true;
+            }
+        }
+        for i in 0..n {
+            if set >> i & 1 == 1 {
+                for &(j, p) in &input[i] {
+                    if people[j - 1] == (p == 0) || !people[j - 1] == (p == 1) {
+                        continue 'out;
+                    }
+                }
+            }
+        }
+        ans = std::cmp::max(ans, count);
     }
-    unimplemented!()
+    println!("{}", ans);
+}
+
+#[inline(always)]
+fn input() -> Vec<Vec<(usize, usize)>> {
+    let n = read_line::<usize>()[0];
+    let mut vec = Vec::new();
+    for _ in 0..n {
+        let mut v = Vec::new();
+        let s = read_line::<usize>()[0];
+        for _ in 0..s {
+            let e = read_line::<usize>();
+            v.push((e[0], e[1]))
+        }
+        vec.push(v)
+    }
+    vec
+}
+
+#[inline]
+fn read_line<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim()
+        .split_whitespace()
+        .map(|c| T::from_str(c).unwrap())
+        .collect()
 }
