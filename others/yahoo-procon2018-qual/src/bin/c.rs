160497492f80d6b35f06b2a22fbc9c4ca13254e5
dentaku <dentaku1412@gmail.com>
2021-10-14 00:04:44 +0900

    Mendokusai

--- others/yahoo-procon2018-qual/src/bin/c.rs
+++ others/yahoo-procon2018-qual/src/bin/c.rs
@@ -1,11 +1,16 @@
-#![allow(unreachable_code)]
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        x: [usize; n],
+        c: [usize; n],
+        v: [usize; n]
     }
-    unimplemented!()
+    let mut dp = vec![None; (1 << n) * n];
+    dp[0] = Some(0);
+    let mut ans = std::i64::MAX;
+    for i in 0..n {}
+    println!("{}", ans);
 }
