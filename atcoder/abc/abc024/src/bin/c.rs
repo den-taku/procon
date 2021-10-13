cd63c3895478a584198be544afd446543f44780d
dentaku <dentaku1412@gmail.com>
2021-10-14 01:33:08 +0900

    AC ABC024 C

--- atcoder/abc/abc024/src/bin/c.rs
+++ atcoder/abc/abc024/src/bin/c.rs
@@ -3,8 +3,32 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
-        t: [[i128; 50];50]
+        _n: usize,
+        d: usize,
+        k: usize,
+        lr: [(usize, usize); d],
+        mut st: [(usize, usize); k]
     }
-    unimplemented!()
+    let mut ans = Vec::with_capacity(k);
+    for (s, t) in st.iter_mut() {
+        for (time, &(l, r)) in lr.iter().enumerate() {
+            if *s < l || r < *s {
+                continue;
+            }
+            if s < t {
+                *s = std::cmp::max(*s, r);
+                if *t <= *s {
+                    ans.push((time + 1).to_string());
+                    break;
+                }
+            } else {
+                *s = std::cmp::min(*s, l);
+                if *s <= *t {
+                    ans.push((time + 1).to_string());
+                    break;
+                }
+            }
+        }
+    }
+    println!("{}", ans.join("\n"))
 }
