aea6f6c3292412f47aff8e9612f865aea0f2df34
dentaku <dentaku1412@gmail.com>
2021-10-17 16:12:49 +0900

    AC ABC051 C

--- atcoder/abc/abc051/src/bin/c.rs
+++ atcoder/abc/abc051/src/bin/c.rs
@@ -1,11 +1,47 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::iter::repeat;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        sx: i32,
+        sy: i32,
+        tx: i32,
+        ty: i32
     }
-    unimplemented!()
+    let mut ans = String::new();
+    // for c in vec!['U'].into_iter().cycle().take((ty - sy) as usize) {
+    //     ans.push(c);
+    // }
+    for c in repeat('U').take((ty - sy) as usize) {
+        ans.push(c);
+    }
+    for c in repeat('R').take((tx - sx) as usize) {
+        ans.push(c);
+    }
+    for c in repeat('D').take((ty - sy) as usize) {
+        ans.push(c);
+    }
+    for c in repeat('L').take((tx - sx) as usize) {
+        ans.push(c);
+    }
+    ans.push('L');
+    for c in repeat('U').take((ty - sy) as usize + 1) {
+        ans.push(c);
+    }
+    for c in repeat('R').take((tx - sx) as usize + 1) {
+        ans.push(c);
+    }
+    ans.push('D');
+    ans.push('R');
+    for c in repeat('D').take((ty - sy) as usize + 1) {
+        ans.push(c);
+    }
+    for c in repeat('L').take((tx - sx) as usize + 1) {
+        ans.push(c);
+    }
+    ans.push('U');
+
+    println!("{}", ans);
 }
