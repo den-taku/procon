3db50a9060c31cce53e14eba67e8fc9a3482c710
dentaku <dentaku1412@gmail.com>
2021-10-27 14:10:58 +0900

    Write ARC013 C

--- atcoder/arc/arc013/src/bin/c.rs
+++ atcoder/arc/arc013/src/bin/c.rs
@@ -1,11 +1,57 @@
 #![allow(unreachable_code)]
-use proconio::{fastout, input};
+use proconio::fastout;
 
 #[fastout]
 fn main() {
-    input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+    let n = read_line::<usize>()[0];
+    let mut bit = 0usize;
+    for _ in 0..n {
+        let (x, y, z) = {
+            let e = read_line::<usize>();
+            (e[0], e[1], e[2])
+        };
+        let m = read_line::<usize>()[0];
+        let mut xs = Vec::new();
+        let mut ys = Vec::new();
+        let mut zs = Vec::new();
+        for _ in 0..m {
+            let (a, b, c) = {
+                let e = read_line::<usize>();
+                (e[0], e[1], e[2])
+            };
+            xs.push(a);
+            ys.push(b);
+            zs.push(c);
+        }
+        xs.sort();
+        ys.sort();
+        zs.sort();
+        for (last, v) in vec![x, y, z].into_iter().zip(vec![xs, ys, zs]) {
+            let mut index = 1;
+            for e in v {
+                bit ^= e - index;
+                index = e + 1;
+            }
+            bit ^= last - index;
+        }
     }
-    unimplemented!()
+    if bit != 0 {
+        println!("WIN")
+    } else {
+        println!("LOSE")
+    }
+}
+
+#[inline]
+fn read_line<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    s.trim()
+        .split_whitespace()
+        .map(|c| T::from_str(c).unwrap())
+        .collect()
 }
