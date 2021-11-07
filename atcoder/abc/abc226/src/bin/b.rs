28e287a49796e000a389b64e2feb84eb3bbdc93e
dentaku <dentaku1412@gmail.com>
2021-11-08 00:43:10 +0900

    Use set instead of vector

--- atcoder/abc/abc226/src/bin/b.rs
+++ atcoder/abc/abc226/src/bin/b.rs
@@ -6,12 +6,15 @@ fn main() {
     input! {
         n: usize,
     }
-    let mut v = Vec::new();
+    // let mut v = Vec::new();
+    let mut set = std::collections::HashSet::new();
     for _ in 0..n {
         input! { l: usize, a: [usize; l] }
-        v.push(a)
+        // v.push(a)
+        set.insert(a);
     }
-    v.sort();
-    v.dedup();
-    println!("{}", v.len())
+    // v.sort();
+    // v.dedup();
+    // println!("{}", v.len())
+    println!("{}", set.len())
 }
