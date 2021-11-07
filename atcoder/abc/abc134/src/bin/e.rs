d914bd00c46652b1419852b02770a77d7f9f0cd4
dentaku <dentaku1412@gmail.com>
2021-11-07 18:57:04 +0900

    AC ABC134 E

--- atcoder/abc/abc134/src/bin/e.rs
+++ atcoder/abc/abc134/src/bin/e.rs
@@ -1,11 +1,21 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::collections::BTreeSet;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        a: [usize; n]
     }
-    unimplemented!()
+    let mut tree = BTreeSet::new();
+    for (i, e) in a.iter().enumerate() {
+        if let Some(&(v, j)) = tree.range(..(e, n - i)).next_back() {
+            tree.remove(&(v, j));
+            tree.insert((e, n - i));
+        } else {
+            tree.insert((e, n - i));
+        }
+    }
+    println!("{}", tree.len());
 }
