9bb48e4307ba6edba3b755fa501512b20a981648
dentaku <dentaku1412@gmail.com>
2021-11-07 19:13:40 +0900

    AC ABC030 C

--- atcoder/abc/abc030/src/bin/b.rs
+++ atcoder/abc/abc030/src/bin/b.rs
@@ -10,10 +10,7 @@ fn main() {
     let minite = 6.0 * m;
     let hour = (30.0 * n + 30.0 * m / 60.0) % 360.0;
     let dis = max(minite, hour) - min(minite, hour);
-    println!("{}", min(
-        dis,
-        360.0 - dis
-    ))
+    println!("{}", min(dis, 360.0 - dis))
 }
 
 fn max(a: f64, b: f64) -> f64 {
--- atcoder/abc/abc030/src/bin/c.rs
+++ atcoder/abc/abc030/src/bin/c.rs
@@ -4,8 +4,29 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        m: usize,
+        x: usize,
+        y: usize,
+        a: [usize; n],
+        b: [usize; m]
     }
-    unimplemented!()
+    let mut time = 0;
+    let mut count = 0;
+
+    loop {
+        let index = a.binary_search(&time).unwrap_or_else(|x| x);
+        if index == a.len() {
+            break;
+        }
+        time = a[index] + x;
+        let index = b.binary_search(&time).unwrap_or_else(|x| x);
+        if index == b.len() {
+            break;
+        }
+        count += 1;
+        time = b[index] + y;
+    }
+
+    println!("{}", count);
 }
