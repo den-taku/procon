7004ef268ab42190249fe13ee33f619f8e807c8a
dentaku <dentaku1412@gmail.com>
2021-11-12 22:36:02 +0900

    AC ABC080 D

--- atcoder/abc/abc080/src/bin/d.rs
+++ atcoder/abc/abc080/src/bin/d.rs
@@ -4,8 +4,40 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        c: usize,
+        stc: [(usize, usize, usize); n]
     }
-    unimplemented!()
+    let mut channels = vec![Vec::new(); c];
+    for (s, t, c) in stc {
+        channels[c - 1].push((s, t))
+    }
+    let mut count = [0; 100_001];
+    for channel in channels.iter_mut() {
+        if channel.is_empty() {
+            continue;
+        }
+        if channel.len() == 1 {
+            count[channel[0].0 - 1] += 1;
+            count[channel[0].1] -= 1;
+            continue;
+        }
+        channel.sort();
+        for ((s1, t1), (s2, _t2)) in channel.windows(2).map(|v| (v[0], v[1])) {
+            count[s1 - 1] += 1;
+            if t1 != s2 {
+                count[t1] -= 1;
+            } else {
+                count[s2 - 1] -= 1;
+            }
+        }
+        count[channel[channel.len() - 1].1] -= 1;
+    }
+    let mut sum = 0;
+    let mut max = 1;
+    for c in count.iter() {
+        sum += c;
+        max = std::cmp::max(max, sum);
+    }
+    println!("{}", max);
 }
