cfddaf354c63d70c6e5e2fdbcc482bd6f06bd9b9
dentaku <dentaku1412@gmail.com>
2021-11-16 23:50:21 +0900

    AC ABC195 E

--- atcoder/abc/abc195/src/bin/e.rs
+++ atcoder/abc/abc195/src/bin/e.rs
@@ -1,11 +1,42 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::collections::HashSet;
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        s: String,
+        x: String,
     }
-    unimplemented!()
+    let s = s
+        .chars()
+        .map(|c| c as u8 - b'0')
+        .map(|c| c as usize)
+        .collect::<Vec<_>>();
+    let x = x.chars().collect::<Vec<_>>();
+    let mut dp = HashSet::new();
+    dp.insert(0);
+    for i in (0..n).rev() {
+        let mut next = HashSet::new();
+        if x[i] == 'T' {
+            for num in 0..7 {
+                if dp.contains(&((10 * num) % 7)) || dp.contains(&((10 * num + s[i]) % 7)) {
+                    next.insert(num);
+                }
+            }
+        } else {
+            for num in 0..7 {
+                if dp.contains(&((10 * num) % 7)) && dp.contains(&((10 * num + s[i]) % 7)) {
+                    next.insert(num);
+                }
+            }
+        }
+        dp = next;
+    }
+    if dp.contains(&0) {
+        println!("Takahashi")
+    } else {
+        println!("Aoki")
+    }
 }
