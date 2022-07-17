02d731d790833488430f0774067d494bd2e57e11
den-taku <dentaku1412@gmail.com>
2022-07-17 21:03:46 +0900

    AC ABC178 E

--- atcoder/abc/abc178/src/bin/e.rs
+++ atcoder/abc/abc178/src/bin/e.rs
@@ -1,11 +1,25 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
+use std::cmp::{max, min};
 
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        n: usize,
+        xys: [(i64, i64); n]
     }
-    unimplemented!()
+    let mut max_sum = std::i64::MIN;
+    let mut min_sum = std::i64::MAX;
+    let mut max_dif = std::i64::MIN;
+    let mut min_dif = std::i64::MAX;
+
+    for (x, y) in xys {
+        let sum = x + y;
+        let dif = x - y;
+        max_sum = max(max_sum, sum);
+        min_sum = min(min_sum, sum);
+        max_dif = max(max_dif, dif);
+        min_dif = min(min_dif, dif);
+    }
+    println!("{}", max(max_sum - min_sum, max_dif - min_dif))
 }
