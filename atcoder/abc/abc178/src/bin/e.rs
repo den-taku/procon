e6021fe1e3d4f16edd71454348c65e83d8075f5d
den-taku <dentaku1412@gmail.com>
2022-07-17 23:21:20 +0900

    Use iterator in order to fasten

--- atcoder/abc/abc178/src/bin/e.rs
+++ atcoder/abc/abc178/src/bin/e.rs
@@ -1,6 +1,5 @@
 #![allow(unreachable_code)]
 use proconio::{fastout, input};
-use std::cmp::{max, min};
 
 #[fastout]
 fn main() {
@@ -8,18 +7,13 @@ fn main() {
         n: usize,
         xys: [(i64, i64); n]
     }
-    let mut max_sum = std::i64::MIN;
-    let mut min_sum = std::i64::MAX;
-    let mut max_dif = std::i64::MIN;
-    let mut min_dif = std::i64::MAX;
-
-    for (x, y) in xys {
-        let sum = x + y;
-        let dif = x - y;
-        max_sum = max(max_sum, sum);
-        min_sum = min(min_sum, sum);
-        max_dif = max(max_dif, dif);
-        min_dif = min(min_dif, dif);
-    }
-    println!("{}", max(max_sum - min_sum, max_dif - min_dif))
+    println!(
+        "{}",
+        std::cmp::max(
+            xys.iter().map(|(x, y)| x + y).max().unwrap()
+                - xys.iter().map(|(x, y)| x + y).min().unwrap(),
+            xys.iter().map(|(x, y)| x - y).max().unwrap()
+                - xys.iter().map(|(x, y)| x - y).min().unwrap(),
+        )
+    );
 }
