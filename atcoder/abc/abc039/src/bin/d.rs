e0d30661a992a29922eab52633211898cbde6123
den-taku <dentaku1412@gmail.com>
2022-07-20 06:05:26 +0900

    AC ABC039D

--- atcoder/abc/abc039/src/bin/d.rs
+++ atcoder/abc/abc039/src/bin/d.rs
@@ -4,8 +4,80 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        _n: usize,
-        _t: [[i128; 50];50]
+        h: usize,
+        w: usize,
+        ss: [String; h]
     }
-    unimplemented!()
+    let mut able = vec![true; h * w];
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.chars().enumerate() {
+            if c == '.' {
+                if i > 0 && j > 0 {
+                    able[(i - 1) * w + (j - 1)] = false;
+                }
+                if i > 0 {
+                    able[(i - 1) * w + j] = false;
+                }
+                if i > 0 && j < w - 1 {
+                    able[(i - 1) * w + j + 1] = false
+                }
+                if j > 0 {
+                    able[i * w + j - 1] = false;
+                }
+                able[i * w + j] = false;
+                if j < w - 1 {
+                    able[i * w + j + 1] = false;
+                }
+                if i < h - 1 && j > 0 {
+                    able[(i + 1) * w + j - 1] = false;
+                }
+                if i < h - 1 {
+                    able[(i + 1) * w + j] = false;
+                }
+                if i < h - 1 && j < w - 1 {
+                    able[(i + 1) * w + j + 1] = false;
+                }
+            }
+        }
+    }
+
+    for (i, s) in ss.iter().enumerate() {
+        for (j, c) in s.chars().enumerate() {
+            if c == '#' {
+                if i > 0 && j > 0 && able[(i - 1) * w + (j - 1)] {
+                    continue;
+                } else if i > 0 && able[(i - 1) * w + j] {
+                    continue;
+                } else if i > 0 && j < w - 1 && able[(i - 1) * w + j + 1] {
+                    continue;
+                } else if j > 0 && able[i * w + j - 1] {
+                    continue;
+                } else if able[i * w + j] {
+                    continue;
+                } else if j < w - 1 && able[i * w + j + 1] {
+                    continue;
+                } else if i < h - 1 && j > 0 && able[(i + 1) * w + j - 1] {
+                    continue;
+                } else if i < h - 1 && able[(i + 1) * w + j] {
+                    continue;
+                } else if i < h - 1 && j < w - 1 && able[(i + 1) * w + j + 1] {
+                    continue;
+                }
+                println!("impossible");
+                return;
+            }
+        }
+    }
+
+    println!("possible");
+    for i in 0..h {
+        for j in 0..w {
+            if able[i * w + j] {
+                print!("#")
+            } else {
+                print!(".")
+            }
+        }
+        println!()
+    }
 }
