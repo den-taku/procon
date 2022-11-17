b26feb2778e0d60771df39e93daceefd28332518
den-taku <dentaku1412@gmail.com>
2022-11-18 00:18:32 +0900

    Check connectivity

--- aoj/aoj003/src/bin/g.rs
+++ aoj/aoj003/src/bin/g.rs
@@ -1,25 +1,105 @@
 #![allow(unreachable_code)]
 
 fn main() {
-    let _n = input();
-    unimplemented!()
+    let mut sc = Scanner::new(std::io::stdin().lock(), 4096);
+    input! {
+        sc = sc,
+        n: usize,
+        m: usize,
+        edges: [(usize, usize); m]
+    }
+
+    let mut adjacent = vec![Vec::new(); n];
+
+    for (u, v) in edges {
+        adjacent[u].push(v);
+        adjacent[v].push(u);
+    }
+
+    let mut visited = vec![false; n];
+    dfs(0, &mut visited, &adjacent);
+    println!(
+        "{}",
+        if visited.iter().all(|&e| e) {
+            "Yes"
+        } else {
+            "No"
+        }
+    )
 }
 
-#[inline(always)]
-fn input() -> usize {
-    read_line::<usize>()[0]
+fn dfs(v: usize, visited: &mut [bool], adjacent: &[Vec<usize>]) {
+    for &u in &adjacent[v] {
+        if !visited[u] {
+            visited[u] = true;
+            dfs(u, visited, adjacent)
+        }
+    }
 }
 
-#[inline(always)]
-fn read_line<T>() -> Vec<T>
-where
-    T: std::str::FromStr,
-    <T as std::str::FromStr>::Err: std::fmt::Debug,
-{
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    s.trim()
-        .split_whitespace()
-        .map(|c| T::from_str(c).unwrap())
-        .collect()
+#[macro_export]
+macro_rules! input{
+    (sc=$sc:expr,$($r:tt)*)=>{
+        input_inner!{$sc,$($r)*}
+    };
+    ($($r:tt)*)=>{
+        let mut sc=fast_input::Scanner::new(std::io::stdin().lock(),4096);
+        input_inner!{sc,$($r)*}
+    };
+}
+
+#[macro_export]
+macro_rules! input_inner{
+    ($sc:expr)=>{};
+    ($sc:expr,)=>{};
+    ($sc:expr,$var:ident:$t:tt$($r:tt)*)=>{
+        let $var=read_value!($sc,$t);
+        input_inner!{$sc $($r)*}
+    };
+}
+
+#[macro_export]
+macro_rules! read_value{
+    ($sc:expr,($($t:tt),*))=>{
+        ($(read_value!($sc,$t)),*)
+    };
+    ($sc:expr,[$t:tt;$len:expr])=>{
+        (0..$len).map(|_|read_value!($sc,$t)).collect::<Vec<_>>()
+    };
+    ($sc:expr,Chars)=>{read_value!($sc,String).chars().collect::<Vec<char>>()};
+    ($sc:expr,Usize1)=>{read_value!($sc,usize)-1};
+    ($sc:expr,$t:ty)=>{$sc.next::<$t>()};
+}
+
+pub struct Scanner {
+    buf: Vec<u8>,
+    pos: usize,
+}
+impl Scanner {
+    pub fn new<R: std::io::Read>(mut reader: R, estimated: usize) -> Self {
+        let mut buf = Vec::with_capacity(estimated);
+        let _ = std::io::copy(&mut reader, &mut buf).unwrap();
+        if buf.last() != Some(&b'\n') {
+            panic!("{}", 0);
+        }
+        Scanner { buf, pos: 0 }
+    }
+    #[inline]
+    pub fn next<T: std::str::FromStr>(&mut self) -> T
+    where
+        T::Err: std::fmt::Debug,
+    {
+        let mut start = None;
+        loop {
+            match (self.buf[self.pos], start.is_some()) {
+                (b' ', true) | (b'\n', true) => break,
+                (_, true) | (b' ', false) | (b'\n', false) | (b'\r', false) => self.pos += 1,
+                (_, false) => start = Some(self.pos),
+            }
+        }
+        let target = &self.buf[start.unwrap()..self.pos];
+        unsafe { std::str::from_utf8_unchecked(target) }
+            .parse()
+            .unwrap()
+    }
 }
